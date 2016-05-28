39cbe97d8136036a73b541960ea8617e22bd18bd
Jean-Michel Gigault <jgigault@student.42.fr>
2016-05-28 18:59:14 +0200

    fix travis compilation

--- incs/builtin.h
+++ incs/builtin.h
@@ -22,13 +22,11 @@ typedef enum				e_builtin_callback
 	BLTIN_CB_AFTER
 }							t_builtin_callback;
 
-typedef int 				(*t_builtin_execute)(t_builtin const *, int, t_sh *, t_proc *);
-
 typedef struct				s_builtin
 {
 	int						index;
 	char const				*name;
-	t_builtin_execute		execute;
+	int						(*exe)(t_builtin const *, int, t_sh *, t_proc *);
 	t_option const 			**options;
 }							t_builtin;
 
@@ -45,7 +43,7 @@ int							builtin_cd(t_builtin const *builtin, int callback, t_sh *sh, t_proc *p
 static const t_builtin		g_builtin_cd = {
 	.index = BLTIN_CD,
 	.name = "cd",
-	.execute = &builtin_cd,
+	.exe = &builtin_cd,
 	.options = g_builtin_cd_options
 };
 
--- srcs/builtins/builtin_callback.c
+++ srcs/builtins/builtin_callback.c
@@ -9,7 +9,7 @@ int	builtin_callback(int callback, t_sh *sh, t_proc *p)
 	{
 		if (ft_strcmp(p->argv[0], g_builtins[i]->name) == 0)
 		{
-			return (g_builtins[i]->execute(g_builtins[i], callback, sh, p));
+			return (g_builtins[i]->exe(g_builtins[i], callback, sh, p));
 		}
 	}
 	return (ST_OK);
