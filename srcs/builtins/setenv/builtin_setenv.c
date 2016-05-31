c746513e88ce27da1614790c9c5b58c81ee9ff4f
gabriel kuma <gkuma@student.42.fr>
2016-06-01 01:57:24 +0200

    update libft, debug setenv set_new_variable

--- srcs/builtins/setenv/builtin_setenv.c
+++ srcs/builtins/setenv/builtin_setenv.c
@@ -12,7 +12,7 @@ static int s_concat_variable(char **str, char *key, char *value)
 		*str = tmp;
 	else if (value)
 	{
-		ft_putendl_fd(value, 1);//debug
+		//ft_putendl_fd(value, 1);//debug
 		tmp2 = ft_strjoin(tmp, value);//checker les retour de strdup MALLOC
 		free(tmp);
 		*str = tmp2;
@@ -26,28 +26,44 @@ static int s_env_set_new_variable(t_sh **sh, char *variable, char *value)
 	char **tmp;
 
 	i = 0;
-	ft_putendl_fd("IN inside env_set_new_variable", 1);//debug
+	 ft_putendl_fd("IN inside env_set_new_variable", 1);//debug
+		caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
 	while ((*sh)->envp[i])
 		i++;
-	tmp = (char **)malloc(sizeof(char *) * i + 2);
+	tmp = (char *)malloc(sizeof(char *) * i + 2);
+	 ft_putendl_fd("IN inside env_set_new_variable1", 1);//debug
+		caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
 	i = 0;
-	ft_putendl_fd("IN inside env_set_new_variable1", 1);//debug
 	while ((*sh)->envp[i])
 	{
-		ft_putnbr_fd(i, 1); // debug
-		ft_putendl_fd((*sh)->envp[i], 1);//debug
-		tmp[i] = ft_strdup((*sh)->envp[i]);
+		// ft_putnbr_fd(i, 1); // debug
+		// ft_putendl_fd((*sh)->envp[i], 1);//debug
+		tmp[i] = (*sh)->envp[i];
 		free((*sh)->envp[i]);
 		i++;
 	}
-	ft_putendl_fd("IN inside env_set_new_variable2", 1);//debug
-//	(void)variable;
-	s_concat_variable(&(tmp[i]), variable, value);
-	ft_putendl_fd("IN inside env_set_new_variable3", 1);//debug
+	free((*sh)->envp);
+	 ft_putendl_fd("IN inside env_set_new_variable2", 1);//debug
+		caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
+	(void)variable;
+	(void)value;
+//	s_concat_variable(&(tmp[i]), variable, value);
+	 ft_putendl_fd("IN inside env_set_new_variable3", 1);//debug
+		caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
 	i++;
 	tmp[i] = NULL;
-	(*sh)->envp = tmp;
-	ft_putendl_fd(" OUT inside env_set_new_variable", 1);//debug	
+	(*sh)->envp = (char **)malloc(sizeof(char *) * i);
+	i = 0;
+	while (tmp[i])
+	{
+		(*sh)->envp[i] = ft_strdup(tmp[i]);
+		free(tmp[i]);
+		i++;
+	}
+	free(tmp);
+	(*sh)->envp[i] = NULL;
+	 ft_putendl_fd(" OUT inside env_set_new_variable", 1);//debug	
+		caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
 	return (ST_OK);
 }
 
@@ -56,7 +72,7 @@ static int s_env_index_value(t_sh *sh, char *variable)
 	int 	i;
 
 	i = 0;
-	ft_putendl_fd("env_index_value", 1);//debug
+//	ft_putendl_fd("env_index_value", 1);//debug
 	while (sh->envp[i])
 	{
 		if (ft_strncmp(sh->envp[i], variable, ft_strlen(variable) - 1) == 0)
@@ -73,17 +89,17 @@ static int s_env_set_variable_and_value(t_sh **sh, t_proc *p)
 
 	ret = 0;
 	i = 0;
-			ft_putendl_fd("set_variable_and_value", 1);//debug
+			// ft_putendl_fd("set_variable_and_value", 1);//debug
 	if ((ret = s_env_index_value(*sh, p->argv[1])) == -1)
 	{
-			ft_putstr_fd("ret", 1);//debug
-			ft_putnbr_fd(ret, 1);//debug
-			ft_putendl_fd("set_variable_and_value1", 1);//debug
+			// ft_putstr_fd("ret", 1);//debug
+			// ft_putnbr_fd(ret, 1);//debug
+			// ft_putendl_fd("set_variable_and_value1", 1);//debug
 		s_env_set_new_variable(sh, p->argv[1], p->argv[2]);
 	}
 	else
 	{
-	ft_putendl_fd("set_variable_and_value2", 1);//debug
+	// ft_putendl_fd("set_variable_and_value2", 1);//debug
 		if (!p->argv[2])
 			s_concat_variable(&(*sh)->envp[ret], p->argv[1], NULL);
 		else if (p->argv[2])
@@ -128,7 +144,7 @@ static int	s_argv_is_valid(t_proc *p)
 	return (ST_OK);
 }
 
-static int	s_before(t_sh **sh, t_proc *p)
+static int	s_before(t_proc *p)
 {
 	int		ret;
 
@@ -142,25 +158,22 @@ static int	s_before(t_sh **sh, t_proc *p)
 			else if (((ret  = s_argv_is_valid(p)) != ST_OK))
 				p->bltin_status = ret;
 			else
-			{
 				p->bltin_status = HAVE_ARGV;
-				s_env_set_variable_and_value(sh, p);
-			}
 		}
 	}
 	return (ST_OK);
 }
 
-static int	s_exec(t_builtin const *builtin, t_proc *p, t_sh **sh)
+static int	s_exec(t_builtin const *builtin, t_proc *p, t_sh *sh)
 {
 	int		i;
 
 	i = 0;
 	if (p->bltin_status == ST_OK)
 	{
-		while ((*sh)->envp[i])
+		while (sh->envp[i])
 		{
-			ft_putendl_fd((*sh)->envp[i], STDOUT_FILENO);
+			ft_putendl_fd(sh->envp[i], STDOUT_FILENO);
 			i++;
 		}	
 	}
@@ -179,23 +192,21 @@ static int	s_exec(t_builtin const *builtin, t_proc *p, t_sh **sh)
 	return (EXIT_SUCCESS);
 }
 
-static int	s_after(t_proc *p)
+static int	s_after(t_sh **sh, t_proc *p)
 {
 
-	if (p->bltin_status == ST_OK)
-	{
-
-	}
+	if (p->bltin_status == HAVE_ARGV)
+		s_env_set_variable_and_value(sh, p);
 	return (ST_OK);
 }
 
 int			builtin_setenv(t_builtin const *builtin, int callback, t_sh *sh, t_proc *p)
 {
 	if (callback == BLTIN_CB_BEFORE)
-		return (s_before(&sh, p));
+		return (s_before(p));
 	if (callback == BLTIN_CB_EXEC)
-		exit(s_exec(builtin, p, &sh));
+		exit(s_exec(builtin, p, sh));
 	if (callback == BLTIN_CB_AFTER)
-		return (s_after(p));
+		return (s_after(&sh, p));
 	return (ST_OK);
 }
