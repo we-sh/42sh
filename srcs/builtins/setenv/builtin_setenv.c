cb209a71a9bdb933c7b4986f80c5bd48d9de2ec9
gabriel kuma <gkuma@student.42.fr>
2016-06-02 15:01:09 +0200

    setenv works withou debug but leaks

--- srcs/builtins/setenv/builtin_setenv.c
+++ srcs/builtins/setenv/builtin_setenv.c
@@ -15,59 +15,12 @@ static int s_concat_variable(char **str, char *key, char *value)
 	}
 	else if (value)
 	{
-		//ft_putendl_fd(value, 1);//debug
 		tmp2 = ft_strjoin(tmp, value);//checker les retour de strdup MALLOC
 		free(tmp);
 		*str = ft_strdup(tmp2);
 		free(tmp2);
 	}
 	return (ST_OK);
-}
-
-static char **s_env_set_new_variable(t_sh **sh, char *variable, char *value)
-{
-	char **tmp;
-	int i;
-	int size;
-
-	i = 0;
-	size = 0;
-
-
-		ft_putendl_fd("IN inside env_set_new_variable", 1);//debug
-		caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-
-
-	while ((*sh)->envp[size])
-		size++;
-	tmp = (char **)malloc(sizeof(char *) * size + 2);
-
-
-		ft_putendl_fd("IN inside env_set_new_variable1", 1);//debug
-		caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-	while ((*sh)->envp[i] != NULL)
-	{
-			// ft_putnbr_fd(i, 1); // debug
-			// ft_putendl_fd((*sh)->envp[i], 1);//debug
-			// caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-
-
-		tmp[i] = ft_strdup((*sh)->envp[i]);
-//		free((*sh)->envp[i]);
-		i++;
-	}
-
-	s_concat_variable(&(tmp[i]), variable, value);
-	i++;
-	//ft_memdel_tab((void ***)&(*sh)->envp);
-	tmp[i] = NULL;
-
-
-		ft_putendl_fd("IN inside env_set_new_variable2", 1);//debug
-		caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-		ft_putendl_fd(" OUT inside env_set_new_variable", 1);//debug	
-		caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-	return (tmp);
 }
 
 static int s_env_index_value(t_sh *sh, char *variable)
@@ -77,7 +30,6 @@ static int s_env_index_value(t_sh *sh, char *variable)
 
 	i = 0;
 	size = ft_strlen(variable);
-//	ft_putendl_fd("env_index_value", 1);//debug
 	while (sh->envp[i])
 	{
 		if (ft_strncmp(sh->envp[i], variable, size) == 0)
@@ -92,60 +44,48 @@ static int s_env_index_value(t_sh *sh, char *variable)
 	return (-1);
 }
 
+static char **s_env_set_new_variable(t_sh **sh, char *variable, char *value)
+{
+	char **tmp;
+	int i;
+	int size;
+
+	i = 0;
+	size = 0;
+	tmp = NULL;
+
+	while ((*sh)->envp[size])
+		size++;
+	tmp = (char **)malloc(sizeof(char *) * size + 2);
+	while ((*sh)->envp[i] != NULL)
+	{
+		tmp[i] = strdup((*sh)->envp[i]);
+		i++;
+	}
+	s_concat_variable(&(tmp[i]), variable, value);
+	ft_putendl_fd(tmp[i], 1);//debug
+	i++;
+	tmp[i] = NULL;
+	return (tmp);
+}
+
+
 static int s_env_set_variable_and_value(t_sh **sh, t_proc *p)
 {
 	int i;
 	int ret;
-	char **tmp;
 
 	ret = 0;
 	i = 0;
-			// ft_putendl_fd("set_variable_and_value", 1);//debug
 	if ((ret = s_env_index_value(*sh, p->argv[1])) == -1)
-	{
-			ft_putendl_fd("set_variable_and_value est  - 1", 1);//debug
-			caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-			ft_putendl_fd(" inside variable_and_value", 1);//debug
-			caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-
-
-		tmp = s_env_set_new_variable(sh, p->argv[1], p->argv[2]);
-		//ft_memdel_tab((void ***)&(*sh)->envp);
-
-			ft_putendl_fd(" inside variable_and_value 2", 1);//debug
-			caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-		(*sh)->envp = tmp;
-			ft_putendl_fd(" inside variable_and_value 3", 1);//debug
-			caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-		while ((*sh)->envp[i])
-		{
-			ft_putendl_fd((*sh)->envp[i], 1);//debug
-			caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-			i++;
-		}
-			ft_putendl_fd(" inside variable_and_value 4", 1);//debug
-			caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-	}
+		(*sh)->envp = s_env_set_new_variable(sh, p->argv[1], p->argv[2]);
 	else
 	{
-			ft_putstr_fd("set_variable_and_value est ", 1);//debug
-			ft_putnbr_fd(ret, 1);//debug
-			ft_putstr_fd("\n", 1);//debug
-			caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-
-
-
 		if (!p->argv[2])
 			s_concat_variable(&(*sh)->envp[ret], p->argv[1], NULL);
 		else if (p->argv[2])
 			s_concat_variable(&(*sh)->envp[ret], p->argv[1], p->argv[2]);
 	}
-
-
-		ft_putendl_fd(" OUT variable_and_value", 1);//debug	
-		caps__print_cap(CAPS__CARRIAGE_RETURN, 0);
-
-
 	return (ST_OK);
 }
 
@@ -201,11 +141,6 @@ static int	s_before(t_proc *p)
 			else
 				p->bltin_status = HAVE_ARGV;
 		}
-	}
-	if (p->bltin_status == HAVE_ARGV)
-	{
-		ft_putendl_fd("Before no error", 1);//debug
-		caps__print_cap(CAPS__CARRIAGE_RETURN, 0);//debug
 	}
 	return (ST_OK);
 }
--- srcs/environment_init.c
+++ srcs/environment_init.c
@@ -18,7 +18,7 @@ int		environment_init(t_sh *sh, char **envp)
 	if (envp[i] == NULL)
 	{
 		s_environment_default(sh->envp);
-		return (1); // Set Default return
+		return (ST_OK); // Set Default return
 	}
 	while (envp[i])
 		i++;
@@ -26,12 +26,11 @@ int		environment_init(t_sh *sh, char **envp)
 	i = 0;
 	while (envp[i])
 	{			
-		 if ((sh->envp[i] = ft_strdup(envp[i])) == NULL)
+		 if ((sh->envp[i] = strdup(envp[i])) == NULL)
 		 {
 		 	log_fatal("ft_strdup failded because of insuffisant memory availale");
 		 	return (-ST_MALLOC);
 		 }
-	//ft_putendl_fd(sh->envp[i], 1);
 		 i++;
 	}
 	sh->envp[i] = NULL;
