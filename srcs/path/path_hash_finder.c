9a454ac6b395a3074ff614eb91a04e80257521a9
gabriel.kuma <gkuma@student.42.fr>
2016-06-13 16:55:46 +0200

    Hash table, Execve everything should works fine norme ok

--- srcs/path/path_hash_finder.c
+++ srcs/path/path_hash_finder.c
@@ -1,7 +1,6 @@
 #include "shell.h"
 
-
-static int s_get_new_cmd(char **commande, char *name, char *path)
+static int				s_get_new_cmd(char **commande, char *name, char *path)
 {
 	char				*tmp;
 	char				*tmp2;
@@ -18,83 +17,98 @@ static int s_get_new_cmd(char **commande, char *name, char *path)
 	return (ST_OK);
 }
 
-int 					path_hash_finder(t_sh *sh, char **commande)
+static int				s_path_check_if_cmd_match_with_folder(char **commande, char *dirname)
 {
 	Fnv64_t				hval;
-	t_hasht				*ptr;
+	char				*tmpcmd;
+
+	tmpcmd = NULL;
+	hval = FNV1A_64_INIT;
+	if (s_get_new_cmd(&tmpcmd, *commande, dirname) == ST_OK)
+	{
+		if (access(tmpcmd, X_OK) != -1)
+		{
+			hval = FNV1A_64_INIT;
+			path_add_folder_content_to_hasht(&hval, *commande, dirname);
+			if (*commande)
+				free(*commande);
+			*commande = ft_strdup(tmpcmd);
+			if (tmpcmd)
+				free(tmpcmd);
+			tmpcmd = NULL;
+			return (ST_OK);
+		}
+		if (tmpcmd)
+			free(tmpcmd);
+		tmpcmd = NULL;
+	}
+	return (-1); //add got return
+}
+
+static int				s_path_commande_not_found_in_hasht(t_sh *sh, char **commande)
+{	
 	char				**folders;
 	char				*tmpcmd;
-	int					index;
 	int					i;
 
 	i = 0;
 	tmpcmd = NULL;
-	hval = fnv_64a_str(*commande, FNV1A_64_INIT);
-	index = (hval%FNV_64_PRIME)%HASH_TABLE_SIZE;
+	folders = ft_strsplit(env_get_path(sh->envp), ':');
+	while (folders[i] != NULL)
+	{
+		if (s_path_check_if_cmd_match_with_folder(commande, folders[i]) != ST_OK)
+			tmpcmd = NULL;
+		i++;
+	}
+	ft_memdel_tab((void ***)&folders);
+	if (tmpcmd == NULL)
+	{		
+		if (access(*commande, X_OK) != -1)
+			return (ST_OK);
+	}
+	return (ST_PATH_NOT_FOUND);
+}
+
+static int 				s_path_iter_in_list(t_hasht *ptr, char **commande)
+{
+	while (ptr->next)
+	{
+		if (ft_strcmp(*commande, ptr->name) == 0 )
+		{
+			if (s_get_new_cmd(commande, ptr->name, ptr->path) != ST_OK)
+				return (ST_MALLOC);
+			return (ST_OK);
+		}
+		ptr = ptr->next;
+	}
+	return (ST_PATH_NOT_FOUND);
+}
+
+int 					path_hash_finder(t_sh *sh, char **commande)
+{
+	int					index;
+
+	index = ((fnv_64a_str(*commande, FNV1A_64_INIT))%FNV_64_PRIME)%HASH_TABLE_SIZE;
 	if (bodies[index].head != NULL && !bodies[index].head->next)
 	{
-		if (s_get_new_cmd(commande,
-							bodies[index].head->name,
-							bodies[index].head->path)
-							!= ST_OK)
+		if (s_get_new_cmd(commande, bodies[index].head->name, bodies[index].head->path)	!= ST_OK)
 			return (ST_MALLOC);
 		else
 			return (ST_OK);
 	}
 	else if (bodies[index].head != NULL && bodies[index].head->next)
 	{
-		ptr = bodies[index].head;
-		while (ptr->next)
-		{
-			if (ft_strcmp(*commande, ptr->name) == 0 )
-			{
-				if (s_get_new_cmd(commande, ptr->name, ptr->path) != ST_OK)
-					return (ST_MALLOC);
-				return (ST_OK);
-			}
-			ptr = ptr->next;
-		}
+		if (s_path_iter_in_list(bodies[index].head, commande) == ST_MALLOC)
+			return (ST_MALLOC);
 	}
 	else
 	{
-		folders = ft_strsplit(env_get_path(sh->envp), ':');
-		while (folders[i] != NULL)
+		if (s_path_commande_not_found_in_hasht(sh, commande) == ST_PATH_NOT_FOUND)
 		{
-			if (s_get_new_cmd(&tmpcmd, *commande, folders[i]) == ST_OK)
-			{
-				log_warn(" Commande test %s", tmpcmd);
-				if (access(tmpcmd, X_OK) != -1)
-				{
-					log_warn(" : Command exist in the path but not in hash table");
-					hval = FNV1A_64_INIT;
-					path_add_folder_content_to_hasht(&hval, *commande, folders[i], &i, &i);
-					if (*commande)
-						free(*commande);
-					*commande = ft_strdup(tmpcmd);
-					log_warn("NEW COMMANDE %s", *commande);
-					if (tmpcmd)
-						free(tmpcmd);
-					tmpcmd = NULL;
-					return (ST_OK);
-				}
-				if (tmpcmd)
-					free(tmpcmd);
-				tmpcmd = NULL;
-			}
-			i++;
-		}
-		if (tmpcmd == NULL)
-		{		
-			log_warn("tmpcmd Value : %s", *commande);
-			if (access(*commande, X_OK) != -1)
-			{
-				log_warn(" : Command exist with absolute : %s", *commande);
-				return (ST_OK);
-			}	
-		}
 			ft_putstr_fd(*commande, sh->fd);
 			ft_putendl_fd(" : Command not found", sh->fd);
-			return (-1); /*USE ST COMMANDE NOT FOUND*/
+			return (ST_OK);
+		}
 	}
 	return (ST_OK);
 }
