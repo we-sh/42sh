0a5635e4ae18d4aa2382b2bb96a5c7e256ade19e
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-15 17:27:24 +0200

    fix builtin exit

--- srcs/builtins/exit/builtin_exit.c
+++ srcs/builtins/exit/builtin_exit.c
@@ -23,28 +23,33 @@ static int	s_exec(t_sh *sh, t_builtin const *builtin, t_proc *p)
 	if (p->bltin_status > ST_OK)
 	{
 		builtin_usage(builtin, p->bltin_status);
-		return (EXIT_FAILURE);
+		return (EXIT_SUCCESS);
 	}
 	if (p->bltin_status < ST_OK)
 		return (-p->bltin_status);
 	else if (p->bltin_status == ST_OK && p->argc == 2)
 		return (EXIT_SUCCESS);
+	else if (p->bltin_status == ST_E2BIG)
+		return (EXIT_FAILURE);
 	return (sh->last_exit_status);
 }
 
-static int	s_after(void)
+static int	s_after(t_sh *sh, t_proc *p)
 {
+	if (p->bltin_status == ST_E2BIG)
+		return (ST_OK);
+	if (p->bltin_status > ST_OK)
+		sh->last_exit_status = EXIT_FAILURE;
 	return (ST_EXIT);
 }
 
 int			builtin_exit(t_builtin const *builtin, int callback, t_sh *sh, t_proc *p)
 {
-	(void)sh;
 	if (callback == BLTIN_CB_BEFORE)
 		return (s_before(p));
 	if (callback == BLTIN_CB_EXEC)
 		exit(s_exec(sh, builtin, p));
 	if (callback == BLTIN_CB_AFTER)
-		return (s_after());
+		return (s_after(sh, p));
 	return (ST_OK);
 }
