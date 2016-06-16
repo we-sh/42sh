e171a002dfc38f2b219fc80d02d7ec7d6458df86
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-06-16 17:18:28 +0200

    splitting function and stub the free part

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -11,37 +11,29 @@ static void	s_job_list_set_default(t_job **j)
 }
 
 /*
-** Entry point of the parser.
-** This function aims to build a list of shell jobs according to a string.
+** Proceed to the parser process according to the structure.
+** TODO remove the execution part of the function.
 */
 
-int			parser(t_sh *sh, const char *in)
+static int	parser_process(t_sh *sh, t_parser *parser)
 {
 	int			st;
-	t_parser	*parser;
 	t_job		*j;
-	t_list		*pos;
-
-	log_success("parser receives input : \"%s\"", in);
+	t_list		*j_ptr;
 
 	s_job_list_set_default(&j);
-	if (!sh || !in || j)
-		return (ST_EINVAL);
-	if ((st = parser_new(&parser, in)) != ST_OK)
-		return (st);
 	if ((st = parser_process_lexer(parser->lexer, parser->in)) != ST_OK)
 		return (st);
-	if ((st = parser_process_build(&g_current_jobs_list_head, parser->lexer, sh->envp)) != ST_OK)
+	if ((st = parser_process_build(&g_current_jobs_list_head,
+					parser->lexer, sh->envp)) != ST_OK)
 		return (st);
 
 	// TODO move it
 	log_warn("execution loop launched into the parser");
-	LIST_FOREACH(&g_current_jobs_list_head, pos)
+	LIST_FOREACH(&g_current_jobs_list_head, j_ptr)
 	{
-		log_trace("%p", &g_current_jobs_list_head);
-		log_trace("%p", CONTAINER_OF(&g_current_jobs_list_head, t_job, list_job));
 		int	exit_status;
-		j = CONTAINER_OF(pos, t_job, list_job);
+		j = CONTAINER_OF(j_ptr, t_job, list_job);
 		exit_status = job_launch(sh, j);
 		if (exit_status != ST_OK)
 		{
@@ -50,4 +42,29 @@ int			parser(t_sh *sh, const char *in)
 		}
 	}
 	return (ST_OK);
+}
+
+/*
+** Entry point of the parser.
+** This function aims to build a list of shell jobs according to a string.
+*/
+
+int			parser(t_sh *sh, const char *in)
+{
+	int			st;
+	t_parser	*parser;
+
+	if (!sh || !in)
+		return (ST_EINVAL);
+
+	log_success("parser receives input : \"%s\"", in);
+
+	if ((st = parser_new(&parser, in)) != ST_OK)
+		return (st);
+
+	st = parser_process(sh, parser);
+	
+	log_warn("run parser callback before exit");
+
+	return (st);
 }
