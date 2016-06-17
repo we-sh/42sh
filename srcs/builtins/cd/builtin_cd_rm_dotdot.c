ed109fd4f027e47d08b15cf032c8fc6909791a06
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-17 14:04:55 +0200

    fix builtin cd with dot

--- srcs/builtins/cd/builtin_cd_rm_dotdot.c
+++ srcs/builtins/cd/builtin_cd_rm_dotdot.c
@@ -1,6 +1,13 @@
 #include "shell.h"
 #include "builtin_cd.h"
 
+static void			s_dot(size_t *i, char **tmp, char **path)
+{
+	*tmp = *path + *i + ((*path)[*i + 1] == '\0' || (*path)[*i + 1] == '/' ? 1 : 2);
+	(*path)[*i] = '\0';
+	*path = ft_strcat(*path, *tmp);
+}
+
 static void			s_dotdot(size_t *i, char **tmp, char **path)
 {
 	*tmp = *path + *i + 2;
@@ -24,7 +31,11 @@ char				*builtin_cd_rm_dotdot(char *path)
 	i = 0;
 	while (path[i] != '\0')
 	{
-		if (path[i] == '.' && path[i + 1] == '.'
+		if (i > 0 && path[i] == '/' && (path[i + 1] == '\0'
+			|| path[i + 1] == '/' || (path[i + 1] == '.' && path[i + 2] == '\0')
+			|| (path[i + 1] == '.' && path[i + 2] == '/')))
+			s_dot(&i, &tmp, &path);
+		else if (path[i] == '.' && path[i + 1] == '.'
 			&& (path[i + 2] == '/' || path[i + 2] == '\0'))
 			s_dotdot(&i, &tmp, &path);
 		else if (path[i] == '.' && (path[i + 1] == '/' || path[i + 1] == '\0'))
