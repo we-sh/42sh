2a01297a9f64f2c4fa38789686d6fbe5d412f2bd
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-06-17 18:58:25 +0200

    implement double redirection right on the model of simple right redirection

--- srcs/parser/token/token_parse_dbl_chev_right.c
+++ srcs/parser/token/token_parse_dbl_chev_right.c
@@ -4,19 +4,53 @@ int	token_parse_dbl_chev_right(t_proc *proc, t_lexer *lexer, int *i)
 {
 	log_trace("entering parsing token %-12s '>>'", "TT_REDIR");
 
-	char *left = lexer->tokens[*i].content;
-	(*i)++;
-	if (lexer->tokens[*i].code != TC_CHEV_RIGHT)
-		log_error("parsing in loosing state");
+	int	left_fd = 0;
+	int	right_fd = 0;
+
+
+	if (lexer->tokens[*i].code == TC_CHEV_RIGHT)
+	{
+		left_fd = STDOUT_FILENO;
+	}
+	else
+	{
+		int left = ft_atoi(lexer->tokens[*i].content);
+		if (left == 0 || left == 1 || left == 2)
+			left_fd = left;
+		else
+			log_error("bad file descriptor");
+		(*i)++;
+	}
+	log_debug("%d", left_fd);
 	(*i)++;
 
-	if (ft_strcmp(lexer->tokens[*i].content, "&") == 0)
+	
+
+
+	if (lexer->tokens[*i].type == TT_SEPARATOR)
 		(*i)++;
 
-	char *right = lexer->tokens[*i].content;
+	if (lexer->tokens[*i].code == TC_AND)
+	{
+		(*i)++;
+
+		int right = ft_atoi(lexer->tokens[*i].content);
+		if (right == 0 || right == 1 || right == 2)
+			right_fd = right;
+		else
+			log_error("bad file descriptor");
+
+	}
+	else
+		right_fd = open(lexer->tokens[*i].content, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	
+	log_debug("fd to redirect : %d", right_fd);
+
+	proc->stdout = right_fd;
+
 	(*i)++;
 
-	log_trace("extracting redire %s > %s", left, right);
-	(void)proc;
+
+
 	return (0);
 }
