cda32f5d430d6c19301f09bf6b7824cc752da2e0
Matthias LECONTE <mleconte@student.42.fr>
2016-06-17 20:32:43 +0200

    TT_ESCAPED is now TT_INHIBITOR + TT_INHIBITOR arent ignored anymore. + Init token_parse_inhib.c

---
+++ srcs/parser/token/token_parse_inhib.c
@@ -0,0 +1,15 @@
+#include "parser.h"
+
+/*
+**
+*/
+
+int		token_parse_inhib(t_proc *proc, t_lexer *lexer, int *i)
+{
+	log_trace("entering parsing token %-12s '|'", "TT_REDIR");
+	(void)proc;
+	(void)lexer;
+	(void)i;
+
+	return (0);
+}
