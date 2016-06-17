b072e134e1847842e1cb985fac7161156bf083a3
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-17 14:04:55 +0200

    fix builtin cd with dot

--- srcs/builtins/cd/builtin_cd.c
+++ srcs/builtins/cd/builtin_cd.c
@@ -24,7 +24,7 @@ static int	s_set_bltin_char(t_sh *sh, t_proc *p)
 		else if (p->argc == 2)
 			p->bltin_char = ft_strjoin3(sh->pwd, "/", p->argv[1]);
 	}
-	if (p->bltin_char == NULL)
+	if (p->bltin_status == ST_OK && p->bltin_char == NULL)
 		return (ST_MALLOC);
 	return (ST_OK);
 }
@@ -41,6 +41,8 @@ static int	s_before(t_sh *sh, t_proc *p)
 		{
 			if ((ret = s_set_bltin_char(sh, p)) != ST_OK)
 				return (ret);
+			if (p->bltin_status != ST_OK)
+				return (ST_OK);
 			if ((p->bltin_char = builtin_cd_rm_dotdot(p->bltin_char)) == NULL)
 				return (ST_MALLOC);
 			p->bltin_status = builtin_cd_chk_path(&p->bltin_char,
@@ -58,12 +60,15 @@ static int	s_exec(t_builtin const *builtin, t_proc *p)
 			builtin_usage(builtin, p->bltin_status);
 		else
 		{
-			if (p->argc == 2 && ft_strcmp(p->argv[1], "-") != 0)
-				display_status(p->bltin_status, NULL, p->argv[1]);
+			if (p->argc == 2 && ft_strcmp(p->argv[1], "-") == 0)
+				display_status(p->bltin_status, "cd",
+					env_get(p->envp, "OLDPWD"));
+			else if (p->argc == 2)
+				display_status(p->bltin_status, "cd", p->argv[1]);
 			else if (p->argc == 1)
-				display_status(p->bltin_status, NULL, env_get(p->envp, "HOME"));
+				display_status(p->bltin_status, "cd", env_get(p->envp, "HOME"));
 			else
-				display_status(p->bltin_status, NULL, NULL);
+				display_status(p->bltin_status, "cd", NULL);
 		}
 		return (EXIT_FAILURE);
 	}
