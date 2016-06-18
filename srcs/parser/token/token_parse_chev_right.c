dd7af31d08c5b18e35070def48f27f7d69cbd1d3
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-06-18 15:23:34 +0200

    better use-case of redirections (status codes ...)

--- srcs/parser/token/token_parse_chev_right.c
+++ srcs/parser/token/token_parse_chev_right.c
@@ -5,70 +5,70 @@
 ** echo tutu > tmp => [ 1 > open(tutu) ]
 ** ls 2>&1 1>/dev/null
 ** ls 1>1
+** TODO generate error token
 */
 
 int	token_parse_chev_right(t_proc *proc, t_lexer *lexer, int *i)
 {
 	log_trace("entering parsing token %-12s '>'", "TT_REDIR");
 
-	// ls > tutu
-	
-	int	left_fd = 0;
-	int	right_fd = 0;
+	int	fd_l;
+	int	fd_r;
 
+	fd_l = 0;
+	fd_r = 0;
 
+	// ls >...
 	if (lexer->tokens[*i].code == TC_CHEV_RIGHT)
+		fd_l = STDOUT_FILENO;
+	else // ls 1>...
 	{
-		left_fd = STDOUT_FILENO;
-	}
-	else
-	{
-		int left = ft_atoi(lexer->tokens[*i].content);
-		if (left == 0 || left == 1 || left == 2)
-			left_fd = left;
-		else
+		fd_l = ft_atoi(lexer->tokens[*i].content);
+		if (fd_l != 0 && fd_l != 1 && fd_l != 2)
 			log_error("bad file descriptor");
 		(*i)++;
 	}
-	log_debug("left fd %d", left_fd);
-
+	// go to redir
 	(*i)++;
 
-	if (lexer->tokens[*i].type == TT_SEPARATOR)
-		(*i)++;
-
+	// ls >&...
 	if (lexer->tokens[*i].code == TC_AND)
 	{
 		(*i)++;
+		// ls >&-
+		if (ft_strcmp(lexer->tokens[*i].content, "-") == 0)
+			log_debug("TO IMPLEMENT");
 
-		int right = ft_atoi(lexer->tokens[*i].content);
-		if (right == 0 || right == 1 || right == 2)
-			right_fd = right;
-		else
+		// ls >&1
+		fd_r = ft_atoi(lexer->tokens[*i].content);
+		if (fd_r != 0 && fd_r != 1 && fd_r != 2)
 			log_error("bad file descriptor");
-
 	}
 	else
-		right_fd = open(lexer->tokens[*i].content, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	
-	log_debug("fd to redirect : %d", right_fd);
-
-
-	if (left_fd == 0)
-		proc->stdin = right_fd;
-	if (left_fd == 1)
-		proc->stdout = right_fd;
-	if (left_fd == 2)
 	{
-		proc->stderr = right_fd;
+		// if separator, escape
+		while (lexer->tokens[*i].type == TT_SEPARATOR)
+			(*i)++;
+		if (lexer->tokens[*i].code != TC_NONE)
+		{
+			log_error("parse error");
+			return (ST_PARSER);
+		}
+		// ls > out
+		fd_r = open(lexer->tokens[*i].content, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	}
+	
 
+	log_debug("fd left : %d - fd right : %d", fd_l, fd_r);
 
-
-
+	if (fd_l == 0)
+		proc->stdin = fd_r;
+	if (fd_l == 1)
+		proc->stdout = fd_r;
+	if (fd_l == 2)
+		proc->stderr = fd_r;
 
 	(*i)++;
-
 	
 	return (0);
 }
