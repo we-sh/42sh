d650944d976ecf9d8dfa3c3350d26f79b3cd688d
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-06-19 17:51:28 +0200

    fixing the norme and some issues

--- srcs/parser/token/token_parse_dbl_chev_right.c
+++ srcs/parser/token/token_parse_dbl_chev_right.c
@@ -1,72 +1,37 @@
 #include "parser.h"
 
-int	token_parse_dbl_chev_right(t_proc *proc, t_lexer *lexer, int *i)
+/*
+ ** Open the file pointed by the arg, return -1 on error and display status.
+ */
+
+static int	s_open_new_fd(char *f)
 {
-	log_trace("entering parsing token %-12s '>>'", "TT_REDIR");
+	int	ret;
 
-	int	fd_l;
-	int	fd_r;
+	if ((ret = open(f, O_WRONLY | O_CREAT | O_APPEND, 0644)) < 0)
+		ft_printf("%s: %s: %s\n", "42sh", f, i18n_translate(ST_OPEN));
+	return (ret);
+}
 
-	fd_l = 0;
-	fd_r = 0;
+static int	s_open_new_fd_int(char *f)
+{
+	int	ret;
 
-	// ls >...
-	if (lexer->tokens[*i].code == TC_DBL_CHEV_RIGHT)
-		fd_l = STDOUT_FILENO;
-	else // ls 1>...
-	{
-		fd_l = ft_atoi(lexer->tokens[*i].content);
-		if (fd_l != 0 && fd_l != 1 && fd_l != 2)
-			log_error("bad file descriptor");
-		(*i)++;
-	}
-	// go to redir
-	(*i)++;
-
-	// ls >&...
-	if (lexer->tokens[*i].code == TC_AND)
-	{
-		(*i)++;
-		// ls >&-
-		if (ft_strcmp(lexer->tokens[*i].content, "-") == 0)
-			return (ST_PARSER);
-		else
-		{
-			// ls >&1
-			log_debug(">>&1");
-			if ((ft_strisnumeric(lexer->tokens[*i].content)) == 0)
-			{
-				log_error("bad file descriptor");
-				return (ST_PARSER);
-			}
-			else
-			{
-				fd_r = ft_atoi(lexer->tokens[*i].content);
-				if (fd_r != 0 && fd_r != 1 && fd_r != 2)
-					log_error("bad file descriptor");
-			}
-		}
-	}
+	if ((ft_strisnumeric(f)) == 0)
+		return (ST_PARSER);
 	else
 	{
-		// if separator, escape
-		while (lexer->tokens[*i].type == TT_SEPARATOR)
-			(*i)++;
-		if (lexer->tokens[*i].code != TC_NONE)
-		{
-			log_error("parse error");
-			return (ST_PARSER);
-		}
-		// ls > out
-		fd_r = open(lexer->tokens[*i].content, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		ret = ft_atoi(f);
+		if (ret != 0 && ret != 1 && ret != 2)
+			log_error("bad file descriptor");
 	}
+	return (ret);
+}
 
-
-	log_debug("fd left : %d - fd right : %d", fd_l, fd_r);
-
+static void	s_set_proc_fds(t_proc *proc, int fd_l, int fd_r)
+{
 	if (!(fd_r == 0 && (fd_l == STDOUT_FILENO || fd_l == STDERR_FILENO)))
 	{
-		// to implement
 		if (fd_l == 0)
 			proc->stdin = fd_r;
 		if (fd_l == 1)
@@ -74,8 +39,44 @@ int	token_parse_dbl_chev_right(t_proc *proc, t_lexer *lexer, int *i)
 		if (fd_l == 2)
 			proc->stderr = fd_r;
 	}
+}
 
+static int	s_parse_right_redir(t_lexer *lexer, int *i)
+{
+	int	fd_r;
+
+	if (lexer->tokens[*i].code == TC_AND)
+		return (ST_PARSER);
+	else
+	{
+		while (lexer->tokens[*i].type == TT_SEPARATOR)
+			(*i)++;
+		if (lexer->tokens[*i].code != TC_NONE)
+			return (ST_PARSER);
+		fd_r = s_open_new_fd(lexer->tokens[*i].content);
+	}
+	return (fd_r);
+}
+
+int			token_parse_dbl_chev_right(t_proc *proc, t_lexer *lexer, int *i)
+{
+	int	fd_l;
+	int	fd_r;
+
+	log_trace("entering parsing token %-12s '>>'", "TT_REDIR");
+	// parse left part of the redirection
+	if (lexer->tokens[*i].code == TC_DBL_CHEV_RIGHT)
+		fd_l = STDOUT_FILENO;
+	else
+	{
+		if ((fd_l = s_open_new_fd_int(lexer->tokens[*i].content)) == ST_PARSER)
+			return (ST_PARSER);
+		(*i)++;
+	}
 	(*i)++;
-
+	if ((fd_r = s_parse_right_redir(lexer, i)) == ST_PARSER)
+		return (ST_PARSER);
+	s_set_proc_fds(proc, fd_l, fd_r);
+	(*i)++;
 	return (0);
 }
