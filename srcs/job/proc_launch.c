1791bc843c07b2b6693bcb0b18a3fd017295e96c
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-20 16:57:21 +0200

    fix proc_launch and redirection + code standard

--- srcs/job/proc_launch.c
+++ srcs/job/proc_launch.c
@@ -4,22 +4,20 @@
 ** This function is the entry point of a child process (freshly forked).
 */
 
-void		proc_launch(t_sh *sh, t_job *j, t_proc *p)
+static int	s_interactive_mode_callback(t_sh *sh, t_job *j, t_proc *p)
 {
-	p->pid = getpid();
 	if (sh->is_interactive == 1)
 	{
 		if (j->pgid == 0)
 			j->pgid = p->pid;
 		if (setpgid(p->pid, j->pgid) == -1)
 		{
-			//log_fatal("setpgid(%d, %d) error: %s", p->pid, j->pgid, strerror(errno));
-			//exit(EXIT_FAILURE);
+			log_warn("setpgid(%d, %d) errno: %d", p->pid, j->pgid, errno);
 		}
 		if (j->foreground == 1)
 			if (tcsetpgrp(sh->fd, j->pgid) == -1)
 			{
-
+				log_warn("setpgid(%d, %d) errno: %d", p->pid, j->pgid, errno);
 			}
 		if (signal_to_default() != ST_OK)
 		{
@@ -27,41 +25,41 @@ void		proc_launch(t_sh *sh, t_job *j, t_proc *p)
 			exit(EXIT_FAILURE);
 		}
 	}
+	return (ST_OK);
+}
 
-
-	if (p->stdin != STDIN_FILENO)
+static int	s_dup2_and_close(int from, int to)
+{
+	if (to != from)
 	{
-		if (p->stdin == -1)
-			close(STDIN_FILENO);
+		if (to == -1)
+			close(from);
 		else
 		{
-			dup2(p->stdin, STDIN_FILENO);
-			close(p->stdin);
+			dup2(to, from);
+			if (!(to == STDERR_FILENO && from == STDOUT_FILENO)
+				&& !(to == STDOUT_FILENO && from == STDERR_FILENO))
+				close(to);
 		}
 	}
-	if (p->stderr != STDERR_FILENO)
-	{
-		if (p->stderr == -1)
-			close(STDERR_FILENO);
-		else
-		{
-			dup2(p->stderr, STDERR_FILENO);
-			if (p->stderr != STDOUT_FILENO)
-				close(p->stderr);
-		}
-	}
-	if (p->stdout != STDOUT_FILENO)
-	{
-		if (p->stdout == -1)
-			close(STDOUT_FILENO);
-		else
-		{
-			dup2(p->stdout, STDOUT_FILENO);
-			if (p->stdout != STDERR_FILENO)
-				close(p->stdout);
-		}	
-	}
+	return (ST_OK);
+}
 
+void		proc_launch(t_sh *sh, t_job *j, t_proc *p)
+{
+	p->pid = getpid();
+	s_interactive_mode_callback(sh, j, p);
+	s_dup2_and_close(STDIN_FILENO, p->stdin);
+	if (p->stdout == STDERR_FILENO)
+	{
+		s_dup2_and_close(STDERR_FILENO, p->stderr);
+		s_dup2_and_close(STDOUT_FILENO, p->stdout);
+	}
+	else
+	{
+		s_dup2_and_close(STDOUT_FILENO, p->stdout);
+		s_dup2_and_close(STDERR_FILENO, p->stderr);
+	}
 	if (p->is_valid == 1)
 	{
 		builtin_callback(BLTIN_CB_EXEC, sh, p);
