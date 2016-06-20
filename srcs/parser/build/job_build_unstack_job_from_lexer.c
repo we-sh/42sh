45ff06c096099727a829b807286e66bd2eb4e225
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-06-20 18:39:18 +0200

    cleaning code

--- srcs/parser/build/job_build_unstack_job_from_lexer.c
+++ srcs/parser/build/job_build_unstack_job_from_lexer.c
@@ -2,10 +2,10 @@
 
 /*
 ** Unstack the list of tokens to build a list of jobs.
-** TODO do not return the job whether it's an empty one
 */
 
-int	job_build_unstack_job_from_lexer(t_job **j, t_lexer *lexer, int *i, char **envp)
+int	job_build_unstack_job_from_lexer(t_job **j, t_lexer *lexer, int *i,
+		char **envp)
 {
 	int		ret;
 	t_proc	*p;
@@ -15,26 +15,27 @@ int	job_build_unstack_job_from_lexer(t_job **j, t_lexer *lexer, int *i, char **e
 	while (*i < lexer->size)
 	{
 		log_info("remaining tokens : %d / %d", lexer->size - *i, lexer->size);
-
 		if ((p = proc_alloc(*j, envp)) == NULL)
 			return (ST_MALLOC);
-
-		if ((ret = ast_unstack_proc_from_lexer(p, lexer, i)) != ST_OK)
+		if ((ret = job_build_unstack_proc_from_lexer(p, lexer, i)) != ST_OK)
 			return (ret);
 		list_push_back(&p->list_proc, &(*j)->proc_head);
 		if (lexer->tokens[*i].type == TT_JOBS)
 		{
-			log_info("end of job encountered     ('%s')", lexer->tokens[*i].content);
+			log_info("end of job encountered     ('%s')",
+					lexer->tokens[*i].content);
 			(*i)++;
 			break ;
 		}
 		else if (lexer->tokens[*i].code == TC_PIPE)
 		{
-			log_info("end of process encountered ('%s')", lexer->tokens[*i].content);
+			log_info("end of process encountered ('%s')",
+					lexer->tokens[*i].content);
 			(*i)++;
 		}
 		else
-			log_info("remaining tokens : %d / %d", lexer->size - *i, lexer->size);
+			log_info("remaining tokens : %d / %d", lexer->size - *i,
+					lexer->size);
 	}
 	return (ST_OK);
 }
