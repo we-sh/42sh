c49bc27a8238d86c0423a13ad6a522e08fe9d6fe
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-21 01:24:18 +0200

    code standard in job_launch.c

--- srcs/job/job_launch.c
+++ srcs/job/job_launch.c
@@ -8,38 +8,51 @@
 
 static int			s_fork_it(t_sh *sh, t_job *j, t_proc *p)
 {
-	int	ret;
+	int		ret;
 
 	if ((ret = env_update_from_cmd_line(&p->argv, &p->argc, &p->envp)) != ST_OK)
-		return (job_kill(sh, j, ret));
+		return (ret);
 	if ((ret = builtin_callback(BLTIN_CB_BEFORE, sh, p)) != ST_OK)
-		return (job_kill(sh, j, ret));
+		return (ret);
 	p->pid = fork();
 	if (p->pid < 0)
-	{
-		log_error("fork failed (%d)", p->pid);
-		return (job_kill(sh, j, ST_FORK));
-	}
+		return (ST_FORK);
 	else if (p->pid == 0)
-	{
 		proc_launch(sh, j, p);
-	}
-	else
+	else if (sh->is_interactive == 1)
 	{
-		if (sh->is_interactive == 1)
+		if (j->pgid == 0)
+			j->pgid = p->pid;
+		if (setpgid(p->pid, j->pgid) < 0)
 		{
-			if (j->pgid == 0)
-				j->pgid = p->pid;
-			if (setpgid(p->pid, j->pgid) < 0)
-			{
-				errno = 0;
-				if (waitpid(p->pid, NULL, WNOHANG) && errno != ECHILD)
-				{
-					return (job_kill(sh, j, ST_SETPGID));
-				}
-			}
+			errno = 0;
+			if (waitpid(p->pid, NULL, WNOHANG) && errno != ECHILD)
+				return (ST_SETPGID);
 		}
 	}
+	return (ST_OK);
+}
+
+static int			s_proc_setup( t_proc *proc, t_sh *sh, t_list *head,int *fds)
+{
+	int		ret;
+
+	if (proc->list_proc.next != head)
+	{
+		if (pipe(fds) < 0)
+			return (ST_PIPE);
+		fds[FD_STDOUT] = fds[FD_PIPE_OUT];
+	}
+	else
+		fds[FD_STDOUT] = proc->j->stdout;
+	if (proc->stdin == proc->j->stdin)
+		proc->stdin = fds[FD_STDIN];
+	if (proc->stdout == proc->j->stdout)
+		proc->stdout = fds[FD_STDOUT];
+	if (proc->stderr == proc->j->stderr)
+		proc->stderr = fds[FD_STDERR];
+	if ((ret = s_fork_it(sh, proc->j, proc)) != ST_OK)
+		return (ret);
 	return (ST_OK);
 }
 
@@ -47,88 +60,48 @@ static int			s_job_setup(t_sh *sh, t_job *job)
 {
 	t_list	*head;
 	t_list	*pos;
-	t_proc	*proc;
-	int		job_pipe[2];
 	int		ret;
+	int		fds[5];
 
-	int in = job->stdin;
-	int out = job->stdout;
-	int err = job->stderr;
-	(void)err;
-
+	fds[FD_STDIN] = job->stdin;
+	fds[FD_STDOUT] = job->stdout;
+	fds[FD_STDERR] = job->stderr;
 	head = &job->proc_head;
 	LIST_FOREACH(head, pos)
 	{
-		proc = CONTAINER_OF(pos, t_proc, list_proc);
-
-		if (pos->next != head)
-		{
-			if (pipe(job_pipe) < 0)
-				return (job_kill(sh, job, ST_PIPE));
-			out = job_pipe[1];
-		}
-		else
-			out = job->stdout;
-
-		if (proc->stdin == job->stdin)
-		{
-			proc->stdin = in;
-		}
-
-		if (proc->stdout == job->stdout)
-		{
-			proc->stdout = out;
-		}
-
-		if (proc->stderr == job->stderr)
-		{
-			proc->stderr = err;
-		}
-
-		ret = s_fork_it(sh, job, proc);
-		if (ret != ST_OK)
-			return (job_kill(sh, job, ret));
-
-		if (in != job->stdin)
-			close(in);
-		if (out != job->stdout)
-			close(out);
-		if (err != job->stderr)
-			close(err);
-		in = job_pipe[0];
-
-		// useful if we need to keep last process alive:
-		//if (p->next == NULL)
-		//	break ;
+		if ((ret = s_proc_setup(CONTAINER_OF(pos, t_proc, list_proc),
+			sh, head, fds)) != ST_OK)
+			return (ret);
+		if (fds[FD_STDIN] != job->stdin)
+			close(fds[FD_STDIN]);
+		if (fds[FD_STDOUT] != job->stdout)
+			close(fds[FD_STDOUT]);
+		if (fds[FD_STDERR] != job->stderr)
+			close(fds[FD_STDERR]);
+		fds[FD_STDIN] = fds[FD_PIPE_IN];
 	}
-
 	return (ST_OK);
 }
 
 int					job_launch(t_sh *sh, t_job *j)
 {
+	log_info("launching job `%s`", j->command);
+
 	int		ret;
 	t_list	*head;
 	t_list	*pos;
 	t_proc	*p;
 
 	j->launched = 1;
-	log_info("launching job `%s`", j->command);
 	ret = s_job_setup(sh, j);
 	if (ret != ST_OK)
-		return (ret);
-
+		return (job_kill(sh, j, ret));
 	if (sh->is_interactive == 0)
 		job_wait(j);
-	else if (j->foreground == 1)
-	{
-		ret = job_foreground(sh, j, 0);
-		if (ret != ST_OK)
-		  return (ret);
-	}
-	else
+	else if (j->foreground == 0)
 		return (job_background(j, 0));
-
+	else if ((ret = job_foreground(sh, j, 0)) != ST_OK)
+		return (ret);
 	head = &j->proc_head;
 	if ((pos = list_nth(head, -1)) != head)
 	{
@@ -138,6 +111,5 @@ int					job_launch(t_sh *sh, t_job *j)
 		if ((ret = builtin_callback(BLTIN_CB_AFTER, sh, p)) != ST_OK)
 			return (ret);
 	}
-
 	return (ST_OK);
 }
