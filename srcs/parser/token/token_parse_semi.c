c8838efa278d6243f6bf6e1b7f0208c2ca6e96b4
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-06-20 19:00:56 +0200

    fixing bad use of st to get parsing error on tt_jobs and add display

--- srcs/parser/token/token_parse_semi.c
+++ srcs/parser/token/token_parse_semi.c
@@ -1,13 +1,15 @@
 #include "parser.h"
 
-/*
-** Return -1 on error, 0 on success, or 1 if a job delimiter is find.
-*/
 int	token_parse_semi(t_proc *proc, t_lexer *lexer, int *i)
 {
 	log_trace("entering parsing token %-12s ';'", "TT_JOBS");
 	(void)lexer;
 	(void)i;
 	(void)proc;
-	return (0);
+	if (proc->argc == 0)
+	{
+		display_status(ST_PARSER, NULL, "unexpected token near ';'");
+		return (ST_PARSER);
+	}
+	return (ST_OK);
 }
