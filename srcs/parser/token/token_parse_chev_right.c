a60b72cff2640e4aaf0f7f3338034be6feecc4e8
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-06-22 18:12:55 +0200

    refactoring right redirection

--- srcs/parser/token/token_parse_chev_right.c
+++ srcs/parser/token/token_parse_chev_right.c
@@ -13,68 +13,68 @@ static int	s_open_new_fd_int(char *f, int *fd)
 	return (ST_OK);
 }
 
-static int	s_parse_right_redir(void *target, t_parser *parser, int *i, int *fd)
+static int	s_parse_right_redir_proc(t_proc *target, t_parser *parser, int *i, int *fd)
 {
 	int		ret;
 	char	*str;
 
 	if (P_TOKEN_CODE(*i) == TC_AND)
 	{
-		log_debug("entering '&'");
-		log_success("%d => `%s'", *i, P_TOKEN_CONTENT(*i));
-		if (parser->mode == F_PARSING_JOBS)
-			token_parse_utils_push_command(P_TOKEN_CONTENT(*i), &((t_job *)target)->command);
-		log_success("%d => `%s'", *i, P_TOKEN_CONTENT(*i));
 		(*i)++;
-		log_success("%d => `%s'", *i, P_TOKEN_CONTENT(*i));
-		log_success("%d => `%s'", (*i) + 1, P_TOKEN_CONTENT((*i) + 1));
 		if (ft_strcmp(P_TOKEN_CONTENT(*i), "-") == 0)
 		{
-			if (parser->mode == F_PARSING_JOBS)
-				token_parse_utils_push_command(P_TOKEN_CONTENT(*i), &((t_job *)target)->command);
-			log_debug("entering '-' close redir");
 			*fd = -1;
 		}
 		else
 		{
-			if (parser->mode == F_PARSING_PROCS)
-				if ((ret = s_open_new_fd_int(P_TOKEN_CONTENT(*i), fd)) != ST_OK)
-				{
-					display_status(ST_PARSER_TOKEN, NULL, P_TOKEN_CONTENT(*i));
-					return (ret);
-				}
+			if ((ret = s_open_new_fd_int(P_TOKEN_CONTENT(*i), fd)) != ST_OK)
+			{
+				display_status(ST_PARSER_TOKEN, NULL, P_TOKEN_CONTENT(*i));
+				return (ret);
+			}
 		}
 	}
 	else
 	{
-		while (P_TOKEN_TYPE(*i) == TT_SEPARATOR
-				|| P_TOKEN_TYPE(*i) == TT_INHIBITOR)
-		{
-			if (parser->mode == F_PARSING_JOBS)
-			{
-				t_job *j;
-				j = (t_job *)target;
-				token_parse_utils_push_command(P_TOKEN_CONTENT(*i), &j->command);
-			}
+		while (P_TOKEN_TYPE(*i) == TT_SEPARATOR || P_TOKEN_TYPE(*i) == TT_INHIBITOR)
 			(*i)++;
-		}
-
 		str = NULL;
 		if ((ret = token_parse_utils_get_full_word(&str, parser->lexer, i)) != ST_OK)
 			return (ret);
+		if ((ret = token_parse_utils_open_new_fd((t_proc *)target, str, fd, O_WRONLY | O_CREAT | O_TRUNC)) != ST_OK)
+			return (ret);
+		free(str);
+	}
+	return (ST_OK);
 
-		if (parser->mode == F_PARSING_JOBS)
-		{
-			t_job *j;
-			j = (t_job *)target;
-			token_parse_utils_push_command(str, &j->command);
-		}
+}
 
-		if (parser->mode == F_PARSING_PROCS)
+static int	s_parse_right_redir_jobs(t_job *target, t_parser *parser, int *i, int *fd)
+{
+	int		ret;
+	char	*str;
+
+	if (P_TOKEN_CODE(*i) == TC_AND)
+	{
+		token_parse_utils_push_command(P_TOKEN_CONTENT(*i), &(target->command));
+		(*i)++;
+		if (ft_strcmp(P_TOKEN_CONTENT(*i), "-") == 0)
 		{
-			if ((ret = token_parse_utils_open_new_fd((t_proc *)target, str, fd, O_WRONLY | O_CREAT | O_TRUNC)) != ST_OK)
-				return (ret);
+			token_parse_utils_push_command(P_TOKEN_CONTENT(*i), &(target->command));
+			*fd = -1;
 		}
+	}
+	else
+	{
+		while (P_TOKEN_TYPE(*i) == TT_SEPARATOR || P_TOKEN_TYPE(*i) == TT_INHIBITOR)
+		{
+			token_parse_utils_push_command(P_TOKEN_CONTENT(*i), &(target->command));
+			(*i)++;
+		}
+		str = NULL;
+		if ((ret = token_parse_utils_get_full_word(&str, parser->lexer, i)) != ST_OK)
+			return (ret);
+		token_parse_utils_push_command(str, &(target->command));
 		free(str);
 	}
 	return (ST_OK);
@@ -102,7 +102,7 @@ static int	s_token_parse_chev_right_proc(t_proc *target, t_parser *parser, t_lex
 		(*i)++;
 	}
 	(*i)++;
-	if ((ret = s_parse_right_redir((void *)target, parser, i, &fd_r)) != ST_OK)
+	if ((ret = s_parse_right_redir_proc((t_proc *)target, parser, i, &fd_r)) != ST_OK)
 		return (ret);
 	token_parse_utils_set_proc_fds(target, fd_l, fd_r);
 	(*i)++;
@@ -129,7 +129,7 @@ static int	s_token_parse_chev_right_jobs(t_job *target, t_parser *parser, t_lexe
 	}
 	token_parse_utils_push_command(TOKEN_CONTENT(*i), &(target->command));
 	(*i)++;
-	if ((ret = s_parse_right_redir((void *)target, parser, i, &fd_r)) != ST_OK)
+	if ((ret = s_parse_right_redir_jobs((t_job *)target, parser, i, &fd_r)) != ST_OK)
 		return (ret);
 	(*i)++;
 	return (ST_OK);
