7d006767ec3613205703471061039e0a9f97c9d1
gabriel.kuma <gkuma@student.42.fr>
2016-06-22 19:47:44 +0200

    Norme done for all termcaps folder exept termcaps_context

--- srcs/termcaps/key__completion.c
+++ srcs/termcaps/key__completion.c
@@ -5,41 +5,58 @@
 #define ENDL_SIZE	(sizeof("\n") - 1)
 #define ENDL				"\n"
 
+static size_t		g_filename_count;
+
+static int		s_incr_y_diff(char *buffer, size_t *buffer_offset,
+								int ref_size, int *y_diff)
+{
+	size_t		filename_by_line;
+
+	filename_by_line = caps__win(WIN_COLUMNS) / ref_size;
+	if (g_filename_count % filename_by_line == 0)
+	{
+		ft_memcpy(buffer + *buffer_offset, ENDL, ENDL_SIZE);
+		*buffer_offset += ENDL_SIZE;
+		*y_diff += 1;
+	}
+	g_filename_count += 1;
+	return (ST_OK);
+}
+
+static int		s_deudeu(char **buffer, size_t *buffer_offset,
+					const int ref_size, t_node_dir *node_dir)
+{
+	*buffer_offset += node_dir->filename.size;
+	ft_memset(*buffer + *buffer_offset, ' ',
+		ref_size - node_dir->filename.size);
+	*buffer_offset += ref_size - node_dir->filename.size;
+	return (ST_OK);
+}
+
 static int		s_fill_buffer(t_list *head,
 								const int ref_size,
 								const size_t buffer_size_max,
 								char *buffer)
 {
 	size_t		buffer_offset;
-	size_t		filename_by_line;
-	size_t		filename_count;
 	t_node_dir	*node_dir;
 	t_list		*pos;
 	int			y_diff;
 	t_list		*safe;
 
 	y_diff = 0;
-	filename_by_line = caps__win(WIN_COLUMNS) / ref_size;
-	filename_count = 0;
 	buffer_offset = 0;
 	safe = head->next;
+	g_filename_count = 0;
 	while ((pos = safe) && (pos != head))
 	{
 		safe = safe->next;
 		node_dir = CONTAINER_OF(pos, t_node_dir, list);
 		ASSERT(buffer_offset + ref_size < buffer_size_max);
-		if (filename_count++ % filename_by_line == 0)
-		{
-			ft_memcpy(buffer + buffer_offset, ENDL, ENDL_SIZE);
-			buffer_offset += ENDL_SIZE;
-			y_diff++;
-		}
+		s_incr_y_diff(buffer, &buffer_offset, ref_size, &y_diff);
 		ft_memcpy(buffer + buffer_offset, node_dir->filename.bytes,
 			node_dir->filename.size);
-		buffer_offset += node_dir->filename.size;
-		ft_memset(buffer + buffer_offset, ' ',
-			ref_size - node_dir->filename.size);
-		buffer_offset += ref_size - node_dir->filename.size;
+		s_deudeu(&buffer, &buffer_offset, ref_size, node_dir);
 	}
 	buffer[buffer_offset] = 0;
 	return (y_diff);
@@ -64,7 +81,7 @@ static int		s_display_completion(t_list *head,
 		free(buffer);
 		return (0);
 	}
-	termcaps_write(fd, buffer, ft_strlen(buffer));
+	write(fd, buffer, ft_strlen(buffer));
 	free(buffer);
 	while (y_diff--)
 		caps__print_cap(CAPS__UP, 0);
@@ -72,9 +89,7 @@ static int		s_display_completion(t_list *head,
 	return (1);
 }
 
-
-
-int		key__completion(t_termcaps_context *context)
+int				key__completion(t_termcaps_context *context)
 {
 	t_buffer	match;
 	t_list		head;
@@ -82,8 +97,8 @@ int		key__completion(t_termcaps_context *context)
 	size_t		list_dir_size;
 	t_node_dir	*node_dir;
 
-	log_fatal("Yoooo entrer key_completion");
 	ref_size = key__completion_list_dir(context, &head, &match) + 1;
+	log_success("ref SIZE %u", ref_size);
 	ASSERT(ref_size != 1);
 	list_dir_size = list_size(&head);
 	if (list_dir_size == 1)
@@ -96,6 +111,5 @@ int		key__completion(t_termcaps_context *context)
 	else
 		s_display_completion(&head, context->fd, list_dir_size, ref_size);
 	list_dir__destroy(&head);
-	log_fatal("Yoooo sortit key_completion");
 	return (1);
 }
--- srcs/termcaps/key__completion_list_dir.c
+++ srcs/termcaps/key__completion_list_dir.c
@@ -1,33 +1,13 @@
 #include "shell.h"
 #include <dirent.h>
 
-static int	s_get_path_and_match(char **out_path,
-								t_buffer *out_match,
-								size_t *cmd_size,
-								char *cmd)
+static int			s_fill_the_match_variable(char **out_path,
+									t_buffer *out_match,
+									size_t *cmd_size,
+									char *cmd)
 {
 	char		*match;
 
-
-
-	cmd[*cmd_size] = 0;
-
-	if (*cmd_size == 0 || cmd[*cmd_size - 1] == ' ')
-	{
-		*out_path = "./";
-		out_match->bytes = NULL;
-		out_match->size = 0;
-		return (1);
-	}
-	while (*cmd_size > 0)
-	{
-		if (ft_isspace(cmd[*cmd_size]))
-		{
-			*cmd_size = *cmd_size + 1;
-			break ;
-		}
-		*cmd_size = *cmd_size - 1;
-	}
 	match = ft_strrchr(cmd + *cmd_size, '/');
 	if (match == NULL)
 	{
@@ -47,57 +27,51 @@ static int	s_get_path_and_match(char **out_path,
 	}
 	return (1);
 }
-size_t		key__completion_list_dir(t_termcaps_context *context,
-									t_list *head,
-									t_buffer *match)
+
+static int			s_get_path_and_match(char **out_path,
+									t_buffer *out_match,
+									size_t *cmd_size,
+									char *cmd)
 {
-
-	char			*path;
-	size_t		cmd_size;
-	char		cmd[1024];
-
-	if (!list_head__command_line_to_buffer(&context->command_line,
-				sizeof(cmd) - 1,
-				&cmd_size,
-				cmd))
+	cmd[*cmd_size] = 0;
+	if (*cmd_size == 0 || cmd[*cmd_size - 1] == ' ')
 	{
-		log_error("list_head__command_line_to_buffer() failed");
-		return (-1);
+		*out_path = "./";
+		out_match->bytes = NULL;
+		out_match->size = 0;
+		return (1);
 	}
+	while (*cmd_size > 0)
+	{
+		if (ft_isspace(cmd[*cmd_size]))
+		{
+			*cmd_size = *cmd_size + 1;
+			break ;
+		}
+		*cmd_size = *cmd_size - 1;
+	}
+	s_fill_the_match_variable(out_path, out_match, cmd_size, cmd);
+	return (1);
+}
 
-	ASSERT(s_get_path_and_match(&path, match, &cmd_size, cmd));
-
-
-
-			log_success("(*match).bytes : %s", (*match).bytes);
-			log_success("PATH : %s", path);
-
+static int			key__completion_search_cmd(t_list *head,
+												char *path,
+												t_buffer *match)
+{
 	DIR				*dp;
 	struct dirent	*ep;
 	t_node_dir		*new;
 	size_t			ref_size;
 
-
 	INIT_LIST_HEAD(head);
-	if ((dp = opendir(path)) == NULL)
-	{
-		log_error("opendir() failed");
-		return (0);
-	}
+	ASSERT((dp = opendir(path)) != NULL);
 	ref_size = 0;
 	while ((ep = readdir(dp)) != NULL)
 	{
 		if ((!(*match).size && ep->d_name[0] == '.') ||
 			ft_memcmp(ep->d_name, (*match).bytes, (*match).size))
-		{
-			log_warn("ep->d_name[0] : %s", ep->d_name);
-			log_success("(*match).bytes : %s", (*match).bytes);
-			log_success("(*match).size : %u", (*match).size);
-
 			continue ;
-		}
-		new = node_dir__create(ep->d_name);
-		if (!new)
+		if (!(new = node_dir__create(ep->d_name)))
 		{
 			list_dir__destroy(head);
 			return (0);
@@ -107,5 +81,27 @@ size_t		key__completion_list_dir(t_termcaps_context *context,
 			ref_size = new->filename.size;
 	}
 	ASSERT(!closedir(dp));
+	return (ref_size);
+}
+
+size_t				key__completion_list_dir(t_termcaps_context *context,
+									t_list *head,
+									t_buffer *match)
+{
+	char			*path;
+	size_t			cmd_size;
+	char			cmd[1024];
+	size_t			ref_size;
+
+	if (!list_head__command_line_to_buffer(&context->command_line,
+				sizeof(cmd) - 1,
+				&cmd_size,
+				cmd))
+	{
+		log_error("list_head__command_line_to_buffer() failed");
+		return (-1);
+	}
+	ASSERT(s_get_path_and_match(&path, match, &cmd_size, cmd));
+	ref_size = key__completion_search_cmd(head, path, match);
 	return (ref_size);
 }
