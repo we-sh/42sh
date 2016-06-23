86325725bef621a69c4b1053aa6c779c6d6cb71b
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-23 17:31:11 +0200

    fix token_parse_separator

--- srcs/parser/token/token_parse_separator.c
+++ srcs/parser/token/token_parse_separator.c
@@ -1,19 +1,21 @@
 #include "shell.h"
 
+int		s_job(t_job *j, t_lexer *lexer, int *i)
+{
+	(void)lexer;
+	if (token_parse_utils_push_command(TOKEN_CONTENT(*i), &j->command) != ST_OK)
+		return (ST_MALLOC);
+	return (ST_OK);
+}
+
 int		token_parse_separator(void *target, t_parser *parser, t_lexer *lexer, int *i)
 {
 	log_trace("entering parsing token %-12s '\'", "TT_SEPARATOR");
-	(void)target;
-	(void)parser;
-	(void)lexer;
-	(void)i;
+
+	int	ret;
 
 	if (parser->mode == F_PARSING_JOBS)
-	{
-		t_job *j;
-		j = (t_job *)target;
-		token_parse_utils_push_command(lexer->tokens[*i].content, &j->command);
-	}
+		ret = s_job((t_job *)target, lexer, i);
 	(*i)++;
 	return (0);
 }
