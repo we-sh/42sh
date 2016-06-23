c58e802cf460e67b49a1c98a9defbac714d2fd41
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-23 15:48:55 +0200

    fix token_parse_utils_skip_separators

--- srcs/parser/token/utils/token_parse_utils_skip_separators.c
+++ srcs/parser/token/utils/token_parse_utils_skip_separators.c
@@ -10,8 +10,7 @@ int	token_parse_utils_skip_separators(t_lexer *lexer, int *i, char **command)
 	while (*i < lexer->size && TOKEN_TYPE(*i) == TT_SEPARATOR)
 	{
 		if (command)
-			if (token_parse_utils_push_command(TOKEN_CONTENT(*i),
-				&j->command) != ST_OK)
+			if (token_parse_utils_push_command(TOKEN_CONTENT(*i), command) != ST_OK)
 				return (ST_MALLOC);
 		(*i)++;
 	}
