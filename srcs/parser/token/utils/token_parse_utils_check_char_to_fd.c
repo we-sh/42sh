574e015461e20e7025f0374c5d94d236a739da92
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-06-23 14:17:27 +0200

    adding description of the function

--- srcs/parser/token/utils/token_parse_utils_check_char_to_fd.c
+++ srcs/parser/token/utils/token_parse_utils_check_char_to_fd.c
@@ -1,5 +1,11 @@
 #include "parser.h"
 
+/*
+** Check whether the char * is the value 0, 1 or 2, and if so, set the fd
+** pointer given in parameter. If the string doesn't statisfy the condition,
+** the file descriptor is unchanged and the function returns ST_PARSER.
+*/
+
 int	token_parse_utils_check_char_to_fd(char *f, int *fd)
 {
 	int	open_fd;
