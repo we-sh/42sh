63afa93ed2bf3f82baef3c8e4678a976457f4b2d
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-06-23 12:12:54 +0200

    adding some comments

--- srcs/parser/token/token_parse_chev_right.c
+++ srcs/parser/token/token_parse_chev_right.c
@@ -2,13 +2,17 @@
 
 static int	s_open_new_fd_int(char *f, int *fd)
 {
+	int	open_fd;
+
 	if ((ft_strisnumeric(f)) == 0)
 		return (ST_PARSER);
 	else
 	{
-		*fd = ft_atoi(f);
-		if (*fd != STDIN_FILENO && *fd != STDOUT_FILENO && *fd != STDERR_FILENO)
+		open_fd = ft_atoi(f);
+		if (open_fd != STDIN_FILENO && open_fd != STDOUT_FILENO
+				&& open_fd != STDERR_FILENO)
 			return (ST_PARSER);
+		*fd = open_fd;
 	}
 	return (ST_OK);
 }
@@ -80,6 +84,20 @@ static int	s_parse_right_redir_jobs(t_job *target, t_parser *parser, int *i, int
 	return (ST_OK);
 }
 
+/*
+** Parse the token 'right arrow' with a t_proc as target.
+** Behavior description :
+** - if the token is '>', the left file descriptor is STDOUT_FILENO.
+** - else, if the token is '&', redirect both STDOUT_FILENO and STDERR_FILENO.
+** - else, if the token is not '&' (eg, 1 for example), check if the given fd is
+** one of 0, 1 or 2. If it fails, the token is redirect to token_parse_none.
+** - if the token is one of 0, 1 or 2, the left fd is set.
+** TODO : set a flag at this step to say that the token was parsed by the arrow.
+** The left fd is set, so now, the function parse the right part of the arrow
+** to set the right file descriptor.
+** The function end by set the proc file descriptors.
+*/
+
 static int	s_token_parse_chev_right_proc(t_proc *target, t_parser *parser, t_lexer *lexer, int *i)
 {
 	int		fd_l;
@@ -99,15 +117,14 @@ static int	s_token_parse_chev_right_proc(t_proc *target, t_parser *parser, t_lex
 			if ((ret = token_parse_none((void *)target, parser, lexer, i)) != ST_OK)
 				return (ret);
 		}
-		(*i)++;
+		(*i)++; // go on arrow
 	}
-	(*i)++;
+	(*i)++; // go token next arrow
 	if ((ret = s_parse_right_redir_proc((t_proc *)target, parser, i, &fd_r)) != ST_OK)
 		return (ret);
 	token_parse_utils_set_proc_fds(target, fd_l, fd_r);
-	(*i)++;
+	(*i)++; // go on token after right part of the redirection
 	return (ST_OK);
-
 }
 
 static int	s_token_parse_chev_right_jobs(t_job *target, t_parser *parser, t_lexer *lexer, int *i)
@@ -135,14 +152,24 @@ static int	s_token_parse_chev_right_jobs(t_job *target, t_parser *parser, t_lexe
 	return (ST_OK);
 }
 
+/*
+** Parse the token 'right arrow'.
+** The function applies some modifications on the target (which may be a t_job
+** or a t_proc) according to the parser and the lexer.
+** Notice that the behavior of the function is different according to target.
+*/
+
 int			token_parse_chev_right(void *target, t_parser *parser, t_lexer *lexer, int *i)
 {
 	int	ret;
 
-	log_trace("entering parsing token %-12s (type: %d) (code: %d)", "TT_REDIR", lexer->tokens[*i].type, lexer->tokens[*i].code);
+	log_trace("entering parsing token %-12s (type: %d) (code: %d) `%s'", "TT_REDIR", TOKEN_TYPE(*i), TOKEN_CODE(*i), TOKEN_CONTENT(*i));
 
 	if (parser->mode == F_PARSING_PROCS)
+	{
+		// p->is_valid
 		ret = s_token_parse_chev_right_proc((t_proc *)target, parser, lexer, i);
+	}
 	else if (parser->mode == F_PARSING_JOBS)
 		ret = s_token_parse_chev_right_jobs((t_job *)target, parser, lexer, i);
 	else
