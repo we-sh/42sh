3b9b8b493498a070413d76525a04fd5feed3232d
gabriel.kuma <gkuma@student.42.fr>
2016-06-24 17:19:42 +0200

    new line when quoting charactere ok, no leaks

--- srcs/quoting/quoting_new_context.c
+++ srcs/quoting/quoting_new_context.c
@@ -62,6 +62,8 @@ static int				s_qloop(t_termcaps_context *c, int quot_value)
 
 
 	char *tmp;
+	char *tmp2;
+	char *tmp3;
 
 	size_t				command_line_cur_size;
 	char				command_line_cur[2048];
@@ -73,42 +75,44 @@ static int				s_qloop(t_termcaps_context *c, int quot_value)
 
 
 	tmp = NULL;
+	tmp2 = NULL;
 	ft_bzero(command_line_cur, 2048);
 	ASSERT(list_head__command_line_to_buffer(&c->command_line,
 											sizeof(command_line_cur) - 1,
 											&command_line_cur_size,
 											command_line_cur));
-
 	if (command_line_cur[0])
 	 	tmp = ft_strdup(command_line_cur);
-	log_warn("VALUE OF CHILD BUFFER  : %s", tmp);
-
-	while ((parser(c->sh, tmp, F_PARSING_TERMCAPS, NULL)) != ST_OK)
+	while ((parser(c->sh, tmp2, F_PARSING_TERMCAPS, NULL)) != ST_OK)
 	{
-	log_success("should enter quoting context with token: %d", ret);
-
 		buff_quote = termcaps_read_input(&child_context);
-
-		log_warn("buff_quote Value : %s", buff_quote);
-
 			if ((test = ft_strjoin("\n", buff_quote)) == NULL)
 			{
 				free(buff_quote);
 				return (ST_MALLOC);
 			}
-
-
-			tmp = ft_strjoin(tmp, test);
-			free(test);
-		// }
-		ft_memdel((void **)&buff_quote); //leaks du child_cntext ou autre i don't knowww
+			if (tmp)
+			{
+				tmp2 = ft_strjoin(tmp, test);
+				free(test);
+				free(tmp);
+				tmp = NULL;
+			}
+			else
+			{
+				tmp3 = ft_strjoin(tmp2, test);
+				free(tmp2);
+				free(test);
+				tmp2 = ft_strdup(tmp3);
+				free(tmp3);
+			}
+		ft_memdel((void **)&buff_quote);
 	}
-	log_success("youyouyou ::::: %s", tmp);
-
 	list_head__command_line_destroy(&c->command_line);
 	list_head__init(&c->command_line);
-	termcaps_string_to_command_line((ft_strlen(tmp)),
-	 								tmp, &c->command_line);
+	termcaps_string_to_command_line((ft_strlen(tmp2)),
+	 								tmp2, &c->command_line);
+	free(tmp2);
 	return (s_d_end(c, &child_context));
 }
 
@@ -117,8 +121,6 @@ int						quoting_new_context(t_termcaps_context *context, int quot_value)
 	int			ret;
 
 	ret = 0;
-	//ret = quoting_invalid(context);
-
 	if (g_in_child == 0)// && (ret != 0))
 	{
 		if ((ret = s_qloop(context, quot_value)) != ST_OK)
