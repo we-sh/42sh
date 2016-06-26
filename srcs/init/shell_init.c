afd62fe5ca4282921583fc271b1835560ade6fc5
Matthias LECONTE <mleconte@student.42.fr>
2016-06-26 22:37:41 +0200

    shell_init.c normed.

--- srcs/init/shell_init.c
+++ srcs/init/shell_init.c
@@ -6,6 +6,22 @@
 ** It returns a negative value when an error occurs.
 */
 
+static int	s_shell_environment(t_sh *sh, char *envp[])
+{
+	int ret;
+
+	if ((ret = shell_environment(sh, envp)) != ST_OK)
+		return (ret);
+	path_init_hasht(sh->envp);
+	if ((ret = conf_file_init(sh->envp)) != ST_OK)
+		return (ret);
+	if ((sh->pwd = getcwd(NULL, 0)) == NULL)
+		return (ST_GETCWD);
+	if ((ret = shell_language(LANG_EN)) < 0)
+		return (-ret);
+	return (ST_OK);
+}
+
 static int	s_shell_fd_init(t_sh *sh)
 {
 	int			fd;
@@ -34,63 +50,65 @@ static int	s_shell_fd_init(t_sh *sh)
 	return (ST_OK);
 }
 
+static int	s_shell_job_control(t_sh *sh)
+{
+	int ret;
+
+	while (1)
+	{
+		ioctl(STDIN_FILENO, TIOCGPGRP, &ret);
+		if (ret == (sh->pgid = getpgrp()))
+			break ;
+		if (kill(-sh->pgid, SIGTTIN) != 0)
+			log_error("kill(-sh->pgid. SIGTTIN) failed");
+	}
+	if ((ret = signal_to_ignore()) != ST_OK)
+		return (ret);
+	if (setpgid(sh->pgid, sh->pgid) < 0)
+		return (ST_SETPGID);
+	log_info("pgid: %d", sh->pgid);
+	if (ioctl(STDIN_FILENO, TIOCSPGRP, &sh->pgid) < 0)
+		return (ST_TCSETPGRP);
+	return (ST_OK);
+}
+
+static int	s_shell_termcaps(t_sh *sh)
+{
+	char	*prompt;
+
+	if (!caps__initialize(sh->fd))
+	{
+		log_fatal("caps__initialize() failed");
+		return (ST_TERMCAPS_INIT);
+	}
+	prompt = shell_set_prompt(sh->envp);
+	if (!termcaps_initialize(sh, prompt, &sh->termcaps_context))
+	{
+		free(prompt);
+		return (ST_TERMCAPS_INIT);
+	}
+	free(prompt);
+	return (ST_OK);
+}
+
 int			shell_init(t_sh *sh, char *envp[])
 {
 	int		ret;
-	char	*prompt;
 
 	INIT_LIST_HEAD(&g_current_jobs_list_head);
 	INIT_LIST_HEAD(&sh->redir_head);
 	sh->last_exit_status = 0;
 	sh->pgid = getpid();
-	/* env */
-	if ((ret = shell_environment(sh, envp)) != ST_OK)
+	if ((ret = s_shell_environment(sh, envp)) != ST_OK)
 		return (ret);
-	path_init_hasht(sh->envp);
-	if ((ret = conf_file_init(sh->envp)) != ST_OK)
-		return (ret);
-	if ((sh->pwd = getcwd(NULL, 0)) == NULL)
-		return (ST_MALLOC);
-
-	if ((ret = shell_language(LANG_EN)) < 0)
-		return (-ret);
 	if ((ret = s_shell_fd_init(sh)) != ST_OK)
-	{
-		log_error("s_shell_fd_init() failed");
 		return (ret);
-	}
 	if (sh->is_interactive == 1)
 	{
-		/* jobs */
-		while (1)
-		{
-			ioctl(STDIN_FILENO, TIOCGPGRP, &ret);
-			if (ret == (sh->pgid = getpgrp()))
-				break ;
-			if (kill(-sh->pgid, SIGTTIN) != 0)
-			  log_error("kill(-sh->pgid. SIGTTIN) failed");
-		}
-		if ((ret = signal_to_ignore()) != ST_OK)
+		if ((ret = s_shell_job_control(sh)) != ST_OK)
 			return (ret);
-		if (setpgid(sh->pgid, sh->pgid) < 0)
-			return (ST_SETPGID);
-		log_info("pgid: %d", sh->pgid);
-
-		if (ioctl(STDIN_FILENO, TIOCSPGRP, &sh->pgid) < 0)
-			return (ST_TCSETPGRP);
-		/* termcaps */
-		if (!caps__initialize(sh->fd))
-		{
-			log_fatal("caps__initialize() failed");
-			return (ST_TERMCAPS_INIT);
-		}
-		prompt = shell_set_prompt(sh->envp);
-		if (!termcaps_initialize(sh, prompt, &sh->termcaps_context))
-		{
-			free(prompt);
-			return (ST_TERMCAPS_INIT);
-		}
-		free(prompt);
+		if ((ret = s_shell_termcaps(sh)) != ST_OK)
+			return (ret);
 	}
 	return (ST_OK);
 }
