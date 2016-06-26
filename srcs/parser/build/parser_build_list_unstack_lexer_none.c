4bdca4d9cd19ef837ba17fad8f704e9f2c4376d8
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-26 23:46:00 +0200

    code standard on unstack_lexer_none.c

--- srcs/parser/build/parser_build_list_unstack_lexer_none.c
+++ srcs/parser/build/parser_build_list_unstack_lexer_none.c
@@ -5,35 +5,41 @@
 ** -> initialize heredoc (dbl_chev_left redirections)
 */
 
-int	parser_build_list_unstack_lexer_none(t_parser *parser, t_lexer *lexer, int *i)
+static int	s_process_tokens(t_parser *parser, t_lexer *lexer, int *i)
 {
 	int		ret;
 
-	// we close old fds and free old list to construct a new one
-	redir_list_free(&parser->sh->redir_head);
+	while (*i < lexer->size
+		&& !(TOKEN_TYPE(*i) == TT_REDIR
+			&& TOKEN_CODE(*i) == TC_DLESS)
+		&& !((*i + 1 < lexer->size
+			&& TOKEN_TYPE(*i) == TT_NAME
+			&& TOKEN_TYPE(*i + 1) == TT_REDIR
+			&& TOKEN_CODE(*i + 1) == TC_DLESS)))
+	{
+		ret = lexer->tokens[*i]->parse(NULL, parser, lexer, i);
+		if (ret != ST_OK)
+			return (ret);
+	}
+	if (*i < lexer->size)
+	{
+		ret = lexer->tokens[*i]->parse(NULL, parser, lexer, i);
+		if (ret != ST_OK)
+			return (ret);
+	}
+	return (ST_OK);
+}
 
+int			parser_build_list_unstack_lexer_none(t_parser *parser,
+				t_lexer *lexer, int *i)
+{
+	int		ret;
+
+	redir_list_free(&parser->sh->redir_head);
 	while (*i < lexer->size)
 	{
-		log_info("remaining tokens (none) : %d / %d", lexer->size - *i, lexer->size);
-		ret = ST_OK;
-
-		while (*i < lexer->size
-			&& !(TOKEN_TYPE(*i) == TT_REDIR && TOKEN_CODE(*i) == TC_DLESS)
-			&& !((*i + 1 < lexer->size && TOKEN_TYPE(*i) == TT_NAME && TOKEN_TYPE(*i + 1) == TT_REDIR && TOKEN_CODE(*i + 1) == TC_DLESS)))
-		{
-			ret = lexer->tokens[*i]->parse(NULL, parser, lexer, i);
-			if (ret != ST_OK)
-				return (ret);
-		}
-
-		if (*i < lexer->size)
-		{
-			log_debug("%d %d", TOKEN_TYPE(*i), TOKEN_CODE(*i));
-			ret = lexer->tokens[*i]->parse(NULL, parser, lexer, i);
-			if (ret != ST_OK)
-				return (ret);
-		}
-
+		if ((ret = s_process_tokens(parser, lexer, i)) != ST_OK)
+			return (ret);
 	}
 	return (ST_OK);
 }
