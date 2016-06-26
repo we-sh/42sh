8580fa172750de9277d2b14751ca0825d7b1db59
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-27 01:11:42 +0200

    code standard on unstack_lexer_proc.c

--- srcs/parser/build/parser_build_list_unstack_lexer_proc.c
+++ srcs/parser/build/parser_build_list_unstack_lexer_proc.c
@@ -4,7 +4,34 @@
 ** Unstack processus from the stack of tokens.
 */
 
-int		parser_build_list_unstack_lexer_proc(t_parser *parser, t_lexer *lexer, int *i)
+static int	s_process_tokens(t_parser *parser, t_lexer *lexer,
+				t_proc *p, int *i)
+{
+	int		ret;
+	while (*i < lexer->size
+		&& !(TOKEN_TYPE(*i) == TT_REDIR && TOKEN_CODE(*i) == TC_PIPE))
+	{
+		ret = lexer->tokens[*i]->parse((void *)p, parser, lexer, i);
+		if (ret != ST_OK)
+		{
+			proc_free(&p);
+			return (ret);
+		}
+	}
+	if (*i < lexer->size)
+	{
+		ret = lexer->tokens[*i]->parse((void *)p, parser, lexer, i);
+		if (ret != ST_OK)
+		{
+			proc_free(&p);
+			return (ret);
+		}
+	}
+	return (ST_OK);
+}
+
+int			parser_build_list_unstack_lexer_proc(t_parser *parser,
+				t_lexer *lexer, int *i)
 {
 	int		ret;
 	t_proc	*p;
@@ -13,35 +40,11 @@ int		parser_build_list_unstack_lexer_proc(t_parser *parser, t_lexer *lexer, int 
 	j = CONTAINER_OF(parser->target_list_head, t_job, proc_head);
 	while (*i < lexer->size)
 	{
-		log_info("remaining tokens (proc) : %d / %d", lexer->size - *i, lexer->size);
-
 		if (!(p = proc_alloc(j)))
 			return (ST_MALLOC);
-
-		while (*i < lexer->size
-			&& !(TOKEN_TYPE(*i) == TT_REDIR && TOKEN_CODE(*i) == TC_PIPE))
-		{
-
-			ret = lexer->tokens[*i]->parse((void *)p, parser, lexer, i);
-			if (ret != ST_OK)
-			{
-				proc_free(&p);
-				return (ret);
-			}
-		}
-
-		if (*i < lexer->size)
-		{
-			ret = lexer->tokens[*i]->parse((void *)p, parser, lexer, i);
-			if (ret != ST_OK)
-			{
-				proc_free(&p);
-				return (ret);
-			}
-		}
-
+		if ((ret = s_process_tokens(parser, lexer, p, i)) != ST_OK)
+			return (ret);
 		list_push_back(&p->list_proc, parser->target_list_head);
-
 	}
 	return (ST_OK);
 }
