412e98cd59e23e2725a8bc266f1ba4c10a8d5549
gabriel kuma <gkuma@student.42.fr>
2016-06-27 19:37:42 +0200

    fix issue mathias mkdir .. cd .. rm ../..

--- srcs/init/shell_set_prompt.c
+++ srcs/init/shell_set_prompt.c
@@ -4,28 +4,47 @@
 ** TODO: Check for leaks and strjoin returns values.
 */
 
+static int s_fill_prompt(char **str, char *buf, char *home)
+{
+	if (buf && (ft_strncmp(buf, home, ft_strlen(home) - 1) == 0))
+	{
+		if ((*str = ft_strjoin3_safe("~", buf + ft_strlen(home), "$> ")) == NULL)
+			return (ST_MALLOC);
+	}
+	else if (buf)
+	{
+		if ((*str = ft_strjoin(buf, "$> ")) == NULL)
+			return (ST_MALLOC);
+	}
+	else
+	{
+		if ((*str = ft_strdup("$> ")) == NULL)
+			return (ST_MALLOC);
+	}
+	return (ST_OK);
+}
+
 char		*shell_set_prompt(char **env)
 {
 	char	*str;
 	char	*buf;
+	int		i;
 	char	*home;
 	char	*tmp;
 
-	if ((buf = getcwd(NULL, 0)) == NULL)
+	i = 0;
+	buf = getcwd(NULL, 0);
+	home = env_get_home(env);
+	if ((s_fill_prompt(&str, buf, home)) == ST_MALLOC)
+	{
+		free(str);
 		return (NULL);
-	if ((home = env_get_home(env)) == NULL)
-		return (NULL);
-	if ((ft_strncmp(buf, home, ft_strlen(home) - 1) == 0))
-		str = ft_strjoin3_safe("~", buf + ft_strlen(home), "$> ");
-	else
-		str = ft_strjoin(buf, "$> ");
-	if (!str)
-		return (NULL);
+	}
 	if ((conf_check_color_mode(env)) == ST_OK)
 	{
-		tmp = ft_strjoin3_safe(ANSI_COLOR_LIGHT_BLUE, str, ANSI_COLOR_RESET);
-		if (!tmp)
-			return (NULL);
+		tmp = ft_strjoin3_safe(ANSI_COLOR_LIGHT_BLUE,
+				str, ANSI_COLOR_RESET);
+			free(str);
 		free(str);
 		str = tmp;
 	}
