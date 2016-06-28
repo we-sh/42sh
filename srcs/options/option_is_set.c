bf1ddbe413505aa79833b8d63b3ad60b72ca6379
Jean-michel GIGAULT <jgigault@student.42.fr>
2016-06-28 13:38:23 +0200

    code standard options

--- srcs/options/option_is_set.c
+++ srcs/options/option_is_set.c
@@ -5,7 +5,8 @@ int	option_is_set(t_list *list_option_head, int option_index)
 	t_list		*opt_pos;
 	t_option	*opt;
 
-	LIST_FOREACH(list_option_head, opt_pos)
+	opt_pos = list_option_head;
+	while ((opt_pos = opt_pos->next) && opt_pos != list_option_head)
 	{
 		opt = CONTAINER_OF(opt_pos, t_option, list_option);
 		if (opt->index == option_index)
