86157855571504735440fb0c97cea53c6730d260
Jean-Michel Gigault <jgigault@student.42.fr>
2016-06-28 14:48:18 +0200

    fix invalid home and conf file

--- srcs/conf/conf_check_color_mode.c
+++ srcs/conf/conf_check_color_mode.c
@@ -9,14 +9,12 @@ static int	s_conf_check_mode(char *content, int fd)
 	{
 		free(content);
 		close(fd);
-		log_success("color is activated.");
 		return (ST_OK);
 	}
 	else
 	{
 		free(content);
 		close(fd);
-		log_success("color is not activated.");
 		return (-1);
 	}
 }
@@ -28,12 +26,14 @@ int			conf_check_color_mode(char **env)
 	char	*path;
 
 	env = NULL;
-	path = ft_strjoin(env_get_home(env), "/.weshrc");
+	if ((content = env_get_home(env)) == NULL)
+		return (ST_OK);
+	if ((path = ft_strjoin3(content, "/", CONFIGURATION_FILE)) == NULL)
+		return (ST_MALLOC);
 	if ((fd = open(path, O_RDONLY)) == -1)
 	{
 		free(path);
-		log_error("open() failed");
-		return (ST_OPEN);
+		return (ST_OK);
 	}
 	free(path);
 	while ((get_next_line(fd, &content)) == 1)
@@ -41,7 +41,6 @@ int			conf_check_color_mode(char **env)
 		if (ft_strncmp(content, "color=", 6) == 0)
 			return (s_conf_check_mode(content, fd));
 	}
-	log_warn("color behaviour is not specified.");
 	free(content);
 	close(fd);
 	return (-1);
