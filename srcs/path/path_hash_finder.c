7a9069f914c7cf415bac12b31343f050821ddcb7
gabriel.kuma <gkuma@student.42.fr>
2016-06-29 21:21:41 +0200

    finder check more cases of error

--- srcs/path/path_hash_finder.c
+++ srcs/path/path_hash_finder.c
@@ -42,24 +42,27 @@ static int		path_look_inside_hast(char **cmd, int index, int *ret)
 {
 	if (bodies[index].head != NULL && !bodies[index].head->next)
 	{
+		*ret = ST_OK;
 		if ((ft_strcmp(bodies[index].head->name, *cmd) != 0))
 			*ret = ST_CMD_NOT_FOUND;
 		else if (path_get_new_cmd(cmd, bodies[index].head->name,
 			bodies[index].head->path) == ST_MALLOC)
 			return (ST_MALLOC);
-		else
-			return (ST_OK);
+		if (*ret == ST_OK && access(*cmd, X_OK) == -1)
+			return (ST_EACCES);
+		return (ST_OK);
 	}
 	else if (bodies[index].head != NULL && bodies[index].head->next)
 	{
+		*ret = ST_OK;
 		if ((*ret = s_path_iter_in_list(bodies[index].head, cmd)) == ST_MALLOC)
-			return (ST_MALLOC);
+			*ret = ST_MALLOC;
 		if (*ret == ST_CMD_NOT_FOUND)
 			return (ST_CMD_NOT_FOUND);
+		if (*ret == ST_OK && access(*cmd, X_OK) == -1)
+			return (ST_EACCES);
 		return (ST_OK);
 	}
-	if (*ret == ST_OK && access(*cmd, X_OK) == -1)
-		return (ST_EACCES);
 	return (*ret);
 }
 
@@ -90,14 +93,15 @@ int				path_hash_finder(char **envp, char **cmd)
 		return (ST_OK);
 	ret = ST_CMD_NOT_FOUND;
 	index = fnv_a_str(*cmd) % HASH_TABLE_SIZE;
-	if ((ret = path_look_inside_hast(cmd, index, &ret)) != ST_CMD_NOT_FOUND)
+	if (ft_strncmp(*cmd, "./", 2) != 0 && 
+		(ret = path_look_inside_hast(cmd, index, &ret)) != ST_CMD_NOT_FOUND)
 		return (ret);
 	if (ft_strncmp(*cmd, "/", 1) != 0 && ft_strncmp(*cmd, ".", 1) != 0)
 	{
 		if ((ret = path_commande_not_found_in_hasht(envp, cmd)) != ST_OK)
 			return (ret);
 	}
-	if (ret == ST_OK && access(*cmd, X_OK) == -1)
+	if (ret != ST_OK && access(*cmd, X_OK) == -1)
 		return (ST_EACCES);
 	if ((ft_strncmp(*cmd, "/", 1) == 0 || ft_strncmp(*cmd, ".", 1) == 0))
 		return (s_path_full(&ret, cmd));
