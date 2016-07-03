6a408143477e5510e5d46f6038f7fac26aee14c5
abombard42 <arthbab@gmail.com>
2016-07-03 16:34:35 +0200

    fix compile error on fnv

--- incs/fnv.h
+++ incs/fnv.h
@@ -49,7 +49,7 @@
 #ifndef FNV_H
 # define FNV_H
 
-# include <sys/types.h>
+# include <stdint.h>
 
 # if ((ULONG_MAX) == (UINT_MAX))
 #  include "fnvhead32.h"
--- incs/fnvhead64.h
+++ incs/fnvhead64.h
@@ -1,10 +1,10 @@
 #ifndef FNVHEAD64_H
 # define FNVHEAD64_H
 
-# define FNV_PRIME	((uint64_t)1099511628211)
+typedef uint64_t	t_fnv;
+
+# define FNV_PRIME	((t_fnv)1099511628211)
 # define FNV0_INIT	((t_fnv)0)
 # define FNV1_INIT	((t_fnv)0xcbf29ce484222325ULL)
-
-typedef uint64_t	t_fnv;
 
 #endif
