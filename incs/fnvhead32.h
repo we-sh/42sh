19f62b7c60d09f11571f772125219ca500e887d7
abombard42 <arthbab@gmail.com>
2016-07-03 16:34:35 +0200

    fix compile error on fnv

--- incs/fnvhead32.h
+++ incs/fnvhead32.h
@@ -1,10 +1,10 @@
 #ifndef FNVHEAD32_H
 # define FNVHEAD32_H
 
-# define FNV_PRIME	((uint32_t)16777619)
+typedef uint32_t	t_fnv;
+
+# define FNV_PRIME	((t_fnv)16777619)
 # define FNV0_INIT	((t_fnv)0)
 # define FNV1_INIT	((t_fnv)0x811c9dc5)
-
-typedef uint32_t	t_fnv;
 
 #endif
