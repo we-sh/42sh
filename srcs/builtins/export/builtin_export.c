a87a953ae6045ca79424651fa1eae316bbbd480d
gabriel kuma <gkuma@student.42.fr>
2016-08-03 08:17:14 +0200

    check all return of malloc functions

--- srcs/builtins/export/builtin_export.c
+++ srcs/builtins/export/builtin_export.c
@@ -34,16 +34,16 @@ static int	s_before(t_sh *sh, t_proc *p)
 	{
 		while(p->argv[i])
 		{
-			tmp = ft_strdup(p->argv[i]);
-			tmp2 = ft_strdup(p->argv[i]);
+			if ((tmp = ft_strdup(p->argv[i])) == NULL)
+				return (ST_MALLOC);
+			if ((tmp2 = ft_strdup(p->argv[i])) == NULL)
+				return (ST_MALLOC);
 			if ((value = env_get_value_and_remove_equal_sign(tmp)) != NULL)
 				builtin_local_var_set_local_loop(&sh, tmp2);
 			free(tmp);
 			free(tmp2);
-			log_info("value of argv %s",p->argv[i]);
 			i++;
 		}
-//		p->bltin_status = ST_DONE;
 	}
 	return (ST_OK);
 }
@@ -71,48 +71,82 @@ static int	s_exec(t_sh *sh, t_builtin const *builtin, t_proc *p)
 	return (ST_OK);
 }
 
-static int	s_after(t_sh **sh, t_proc *p)
+static int	s_builtin_export_n_option(t_sh **sh, t_proc *p)
+{
+	char *value;
+	char *tmp;
+	int i;
+	
+	i = 1;
+	while (p->argv[i])
+	{
+		if ((tmp = ft_strdup(p->argv[i])) == NULL)
+			return (ST_MALLOC);
+		if ((value = env_get_value_and_remove_equal_sign(tmp)) != NULL)
+			env_unset(&(*sh)->envp, tmp);
+		else
+			env_unset(&(*sh)->envp, p->argv[i]);
+		free(tmp);
+		i++;
+	}
+	return (ST_OK);
+}
+
+static int s_builtin_export_set(t_sh **sh, t_proc *p)
 {
 	char *value;
 	char *tmp;
 	int ret;
 	int i;
-	int j;
 
 	i = 1;
-	j = 0;
 	ret = 0;
+	while (p->argv[i])
+	{
+		if ((value = local_var_register(*sh, p->argv[i])) != NULL)
+		{
+			if ((tmp = ft_strjoin3_safe(p->argv[i],"=",value)) == NULL)
+				return (ST_MALLOC);
+			if ((builtin_local_var_set_local_loop(sh, tmp)) == ST_MALLOC)
+				return (ST_MALLOC);
+			if ((ret = env_set(&(*sh)->envp, p->argv[i], value)) != ST_OK)
+				return (ret);
+			free(tmp);
+		}
+		else
+		{
+			if ((tmp = ft_strdup(p->argv[i])) == NULL)
+				return (ST_MALLOC);
+			if ((value = env_get_value_and_remove_equal_sign(tmp)) != NULL)
+			{
+				if ((builtin_local_var_set_local_loop(sh, p->argv[i])) == ST_MALLOC)
+					return (ST_MALLOC);
+				if ((ret = env_set(&(*sh)->envp, tmp, value)) != ST_OK)
+					return (ret);
+			}
+			free(tmp);
+		}
+		i++;
+	}
+	return (ST_OK);
+}
+
+static int	s_after(t_sh **sh, t_proc *p)
+{
+	int i;
+
+	i = 1;
 	if (p->bltin_status == ST_OK)
 	{
-		while (p->argv[i])
+		if ((option_is_set(&p->bltin_opt_head, ST_BLTIN_EXPORT_OPT_N)) == 1)
 		{
-			log_info("LA VALEUR DE ARGV[I]:%s", p->argv[i]);
-			if ((value = local_var_register(*sh, p->argv[i])) != NULL)
-			{
-				log_warn("1@ key: %s , Value: %s", p->argv[i], value);
-				tmp = ft_strjoin3_safe(p->argv[i],"=",value);
-				if ((builtin_local_var_set_local_loop(sh, tmp)) == ST_MALLOC)
-					return (ST_MALLOC);
-				if ((ret = env_set(&(*sh)->envp, p->argv[i], value)) != ST_OK)
-					return (ret);
-				free(tmp);
-				(void)sh;
-			}
-			else
-			{
-				tmp = ft_strdup(p->argv[i]);
-				if ((value = env_get_value_and_remove_equal_sign(tmp)) != NULL)
-				{
-					log_warn("2@ key: %s , Value: %s", tmp, value);
-					if ((builtin_local_var_set_local_loop(sh, p->argv[i])) == ST_MALLOC)
-						return (ST_MALLOC);
-					if ((ret = env_set(&(*sh)->envp, tmp, value)) != ST_OK)
-						return (ret);
-					log_warn("Value of SET: %d", ret);
-				}
-				free(tmp);
-			}
-			i++;
+			if ((s_builtin_export_n_option(sh, p)) == ST_MALLOC)
+				return (ST_MALLOC);
+		}
+		else
+		{
+			if ((s_builtin_export_set(sh, p)) == ST_MALLOC)
+				return (ST_MALLOC);
 		}
 	}
 	return (ST_OK);
