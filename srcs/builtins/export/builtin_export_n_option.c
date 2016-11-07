3d2d40c2f1fd83b20f6131ecf16b88db2f295934
Gabriel KUMA <gkuma@e2r4p20.42.fr>
2016-11-07 18:35:54 +0100

    norme 0.1

--- srcs/builtins/export/builtin_export_n_option.c
+++ srcs/builtins/export/builtin_export_n_option.c
@@ -1,34 +1,28 @@
 #include "shell.h"
 
-int						s_builtin_local_var_delete(t_sh **sh, char *key)
+int			s_builtin_local_var_delete(t_sh **sh, char *key)
 {
-	t_var				*ptrvar;
+	t_var	*ptrvar;
 
-	log_success("Inside 1 ");
 	if (!((*sh)->local_vars))
 		return (ST_OK);
 	ptrvar = (*sh)->local_vars;
-	log_success("Inside 2: %s ", ptrvar->key);
 	if (ft_strcmp(ptrvar->key, key) == 0)
 	{
-		log_success("Inside 3: %s ", ptrvar->key);
-		log_success("Inside 3 next: %s ", ptrvar->next);
-    if (ptrvar->next)
-      (*sh)->local_vars = ptrvar->next;
-    else
-      (*sh)->local_vars = NULL;
+		if (ptrvar->next)
+			(*sh)->local_vars = ptrvar->next;
+		else
+			(*sh)->local_vars = NULL;
 		free(ptrvar);
 		return (ST_OK);
 	}
 	while (ptrvar->next)
 	{
-	log_success("Inside 3: %s ", ptrvar->next->key);
 		if (ft_strcmp(ptrvar->next->key, key) == 0)
 		{
 			ptrvar->next = ptrvar->next->next;
-			log_success("Remove ptrvar->next: %s", ptrvar->next);
 			free(ptrvar->next);
-			break;
+			break ;
 		}
 		ptrvar = ptrvar->next;
 	}
@@ -39,18 +33,16 @@ int			builtin_export_n_option(t_sh **sh, char *argv)
 {
 	char	*value;
 	char	*tmp;
-	
+
 	if ((tmp = ft_strdup(argv)) == NULL)
 		return (ST_MALLOC);
 	if ((value = env_get_value_and_remove_equal_sign(tmp)) != NULL)
 	{
-		log_success("Remove TMP2: %s", tmp);
 		env_unset(&(*sh)->envp, tmp);
 		s_builtin_local_var_delete(sh, tmp);
 	}
 	else
 	{
-		log_success("Remove TMP1: %s", tmp);
 		env_unset(&(*sh)->envp, argv);
 		s_builtin_local_var_delete(sh, tmp);
 	}
