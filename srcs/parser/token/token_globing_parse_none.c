6c61f86138f7fb763a48f7dbf91c909d82974e87
Gabriel KUMA <gkuma@e1r12p15.42.fr>
2016-11-08 14:56:07 +0100

    norme done, all tests works

--- srcs/parser/token/token_globing_parse_none.c
+++ srcs/parser/token/token_globing_parse_none.c
@@ -35,44 +35,44 @@ static int	s_suite(t_parser *parser, char *output)
 	return (ret);
 }
 
-static 		int s_istilde(char *input)
+static int	s_replace_tilde(t_parser *parser, void *target, char *output)
 {
-	if (input[0] == '~' && (input[1] == '\0' || input[1] == '/'))
-		return (1);
-	return (0);
+	t_argv	*argument;
+	char	*tmp;
+	char	*tmp2;
+
+	argument = (t_argv *)target;
+	tmp = env_get_home(parser->sh->envp);
+	if (tmp)
+	{
+		tmp2 = argument->buffer;
+		if ((argument->buffer = ft_strjoin(tmp, output + 1)) == NULL)
+			return (ST_MALLOC);
+		free(tmp2);
+	}
+	return (ST_OK);
 }
 
 int			token_globing_parse_none(void *target, t_parser *parser,
 				t_lexer *lexer, int *i)
 {
-	t_argv	*argument;
-	char	*tmp;
-	char	*tmp2;
 	int		ret;
 	char	*output;
 
 	ret = ST_OK;
-	argument = (t_argv *)target;
-	log_warn("Token GLOBING PARSE-NONE debug lexer number %d", lexer->size);
-	ret = 0;
 	output = NULL;
-	if((ret = local_var_replace(parser->sh, TOKEN_CONTENT(*i), &output)) != ST_OK)
+	if ((ret = local_var_replace(parser->sh, TOKEN_CONTENT(*i), &output))
+		!= ST_OK)
 		return (ret);
-	if (*i == 0 && s_istilde(output))
+	if (*i == 0 &&
+		(output[0] == '~' && (output[1] == '\0' || output[1] == '/')))
 	{
-		tmp = env_get_home(parser->sh->envp);
-		if (tmp)
-		{
-			tmp2 = argument->buffer;
-			if ((argument->buffer = ft_strjoin(tmp, output + 1)) == NULL)
-				return (ST_MALLOC);
-			free(tmp2);
-		}
+		if ((s_replace_tilde(parser, target, output)) == ST_MALLOC)
+			return (ST_MALLOC);
 	}
 	else
 		ret = s_suite(parser, output);
 	(*i)++;
 	free(output);
-	log_success("EXIT New input %s", parser->in);
 	return (ret);
 }
