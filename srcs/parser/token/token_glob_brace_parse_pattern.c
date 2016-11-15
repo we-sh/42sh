222cf681d1a587451d38dbc9ddefc9f74769692f
Jean-Michel Gigault <jgigault@student.42.fr>
2016-11-15 22:50:44 +0100

    glob_brace ASCII range

--- srcs/parser/token/token_glob_brace_parse_pattern.c
+++ srcs/parser/token/token_glob_brace_parse_pattern.c
@@ -2,7 +2,8 @@
 
 #define T_PATTERN_NONE 0
 #define T_PATTERN_LIST 1
-#define T_PATTERN_RANGE 2
+#define T_PATTERN_NUMERIC_RANGE 2
+#define T_PATTERN_ASCII_RANGE 3
 
 static int	s_get_pattern_check_token(int pattern_type, t_lexer *lexer,
 										int index, int i)
@@ -11,7 +12,8 @@ static int	s_get_pattern_check_token(int pattern_type, t_lexer *lexer,
 
 	if (TOKEN_CODE(index + i) == TC_COMMA)
 		pattern_type = T_PATTERN_LIST;
-	if (pattern_type == T_PATTERN_RANGE)
+	if (pattern_type == T_PATTERN_ASCII_RANGE
+		|| pattern_type == T_PATTERN_NUMERIC_RANGE)
 	{
 		if (index % 2 == 0)
 		{
@@ -21,6 +23,8 @@ static int	s_get_pattern_check_token(int pattern_type, t_lexer *lexer,
 			if (!(is_numeric == 1 || (ft_strlen(TOKEN_CONTENT(index + i)) == 1
 								&& ft_isascii(TOKEN_CONTENT(index + i)[0]))))
 				pattern_type = T_PATTERN_NONE;
+			if (index == 0 && is_numeric == 1)
+				pattern_type = T_PATTERN_NUMERIC_RANGE;
 			if (index == 2 && (ft_strisnumeric(TOKEN_CONTENT(i)) != is_numeric))
 				return (T_PATTERN_NONE);
 		}
@@ -38,7 +42,7 @@ static int	s_get_pattern(t_lexer *lexer, int i)
 	if (i >= lexer->size - 1)
 		return (T_PATTERN_NONE);
 	index = 0;
-	pattern_type = T_PATTERN_RANGE;
+	pattern_type = T_PATTERN_ASCII_RANGE;
 	while (index + i < lexer->size && TOKEN_CODE(index + i) != TC_RBRACE)
 	{
 		pattern_type = s_get_pattern_check_token(pattern_type, lexer, index, i);
@@ -46,7 +50,8 @@ static int	s_get_pattern(t_lexer *lexer, int i)
 			return (T_PATTERN_NONE);
 		index++;
 	}
-	if (pattern_type == T_PATTERN_RANGE && index > 3)
+	if ((pattern_type == T_PATTERN_ASCII_RANGE
+		|| pattern_type == T_PATTERN_NUMERIC_RANGE) && index > 3)
 		return (T_PATTERN_NONE);
 	if (TOKEN_CODE(index + i) != TC_RBRACE)
 		return (T_PATTERN_NONE);
@@ -70,8 +75,10 @@ int			token_glob_brace_parse_pattern(void *target, t_parser *parser,
 														TOKEN_CONTENT(*i));
 	else
 	{
-		if (pattern_type == T_PATTERN_RANGE)
+		if (pattern_type == T_PATTERN_NUMERIC_RANGE)
 			ret = token_glob_brace_parse_numeric_range(parser, argv_list, i);
+		else if (pattern_type == T_PATTERN_ASCII_RANGE)
+			ret = token_glob_brace_parse_ascii_range(parser, argv_list, i);
 	}
 	(*i)++;
 	return (ret);
---
+++ srcs/parser/token/utils/token_glob_brace_parse_ascii_range.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+
+static int		s_update_buffer_itoa(t_argv *el, char *original, int value)
+{
+	char	*tmp;
+
+	if ((tmp = ft_strnew(1)) == NULL)
+		return (ST_MALLOC);
+	tmp[0] = (char)value;
+	if (el->buffer != NULL)
+		free(el->buffer);
+	if ((el->buffer = ft_strjoin(original, tmp)) == NULL)
+		return (ST_MALLOC);
+	free(tmp);
+	return (ST_OK);
+}
+
+static t_argv	*s_alloc_new_argv(t_list *prev)
+{
+	t_argv	*argument;
+
+	if ((argument = (t_argv *)malloc(sizeof(t_argv))) == NULL)
+		return (NULL);
+	argument->buffer = NULL;
+	argument->argv_list.prev = prev;
+	argument->argv_list.next = NULL;
+	return (argument);
+}
+
+static t_argv	*s_iterate_on_arguments(t_argv *argument, t_list *pos,
+										int start_original, int end)
+{
+	int		start;
+	int		dir;
+	t_list	*prev;
+	char	*original_str;
+
+	if ((original_str = ft_strdup(argument->buffer)) == NULL)
+		return (NULL);
+	prev = pos;
+	start = start_original;
+	dir = start > end ? -1 : 1;
+	while (1)
+	{
+		if (start != start_original)
+			if ((argument =  s_alloc_new_argv(prev)) == NULL)
+				return (NULL);
+		if (s_update_buffer_itoa(argument, original_str, start) != ST_OK)
+			return (NULL);
+		prev->next = &argument->argv_list;
+		if (start == end)
+			break ;
+		start += dir;
+		prev = &argument->argv_list;
+	}
+	free(original_str);
+	return (argument);
+}
+
+int				token_glob_brace_parse_ascii_range(t_parser *parser,
+											t_list *pos, int *i)
+{
+	int		start_original;
+	int		end;
+	t_list	*next;
+	t_argv	*last_new;
+
+	start_original = (int)P_TOKEN_CONTENT(*i + 1)[0];
+	end = (int)P_TOKEN_CONTENT(*i + 3)[0];
+	while ((pos = pos->next) && pos != parser->target_list_head)
+	{
+		next = pos->next;
+		last_new = s_iterate_on_arguments(CONTAINER_OF(pos, t_argv, argv_list),
+													pos, start_original, end);
+		if (last_new == NULL)
+			return (ST_MALLOC);
+		pos = &last_new->argv_list;
+		last_new->argv_list.next = next;
+		next->prev = &last_new->argv_list;
+	}
+	*i += 4;
+	return (ST_OK);
+}
