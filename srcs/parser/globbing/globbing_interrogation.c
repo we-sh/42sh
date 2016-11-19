012e543e0b598bc8651326b41176361c73b0a954
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-11-19 17:32:27 +0100

    support for globbing on other directories

--- srcs/parser/globbing/globbing_interrogation.c
+++ srcs/parser/globbing/globbing_interrogation.c
@@ -36,6 +36,102 @@ int check_globbing(char *pattern, char *input)
         return (*input == *pattern++ && ((*input++ == '\0') || check_globbing(pattern,input)));
 	}
 }
+	/* Context
+	 *
+	 * if ../../?/?
+	 *
+	 * 1) left context is ../../
+	 *
+	 * perform matching on left context + middle
+	 * and join the end after
+	 *
+	 * middle is ?
+	 * right context is /?
+	 *
+	 * 2) ../../x/?
+	 *
+	 * left context is ../../x
+	 * middle is ?
+	 * right is null
+	 *
+	 * peform matching on left context + middle
+	 *
+	 * while right context is not empty
+	 *
+	 *
+	 */
+
+/*
+** no root = 0
+** root = 1
+*/
+char *s_get_left_ctx(char **arg, int is_root)
+{
+	char *ret = NULL;
+
+	int i = 0;
+
+	if (is_root == 1)
+		ret = ft_strdup("/");
+	while (arg[i])
+	{
+		log_trace("%s", arg[i]);
+		if (ft_strchr(arg[i], '*') == NULL && ft_strchr(arg[i], '?') == NULL)
+		{
+			if (ret == NULL)
+				ret = ft_strdup(arg[i]);
+			else
+				ret = ft_strjoin(ret, arg[i]);
+			ret = ft_strjoin(ret, "/");
+		}
+		else
+		{
+			return (ret);
+
+		}
+		log_trace("%s", arg[i]);
+		i++;
+	}
+	return (ret);
+}
+
+char *s_get_ctx(char **arg)
+{
+	int i = 0;
+	while (arg[i])
+	{
+		if (ft_strchr(arg[i], '*'))
+			return (ft_strdup(arg[i]));
+		if (ft_strchr(arg[i], '?'))
+			return (ft_strdup(arg[i]));
+		i++;
+	}
+	return (NULL);
+}
+
+char *s_get_right_ctx(char **arg)
+{
+	char *ret = NULL;
+
+	int i = 0;
+
+	int star_detected = 0;
+	while (arg[i])
+	{
+		if (star_detected == 1)
+		{
+			if (ret == NULL)
+				ret = ft_strdup("/");
+			else
+				ret = ft_strjoin(ret, "/");
+			ret = ft_strjoin(ret, arg[i]);
+		}
+		if (ft_strchr(arg[i], '*') != NULL || ft_strchr(arg[i], '?') != NULL )
+			star_detected = 1;
+		i++;
+	}
+	return (ret);
+}
 
 /*
 ** This function create the list of what it must be compared
@@ -46,25 +142,62 @@ int s_globbing_interrogation_parse_argument(char *arg, t_list *list_globbing)
 {
 	DIR				*dp;
 	struct dirent	*ep;
+	
+	log_debug("globbing on %s", arg);
 
-	if ((dp = opendir ("."))) {
+
+	char **sp = ft_strsplit(arg, '/');
+
+	char *left_ctx;
+	if (arg[0] == '/')
+		left_ctx = s_get_left_ctx(sp, 1);
+	else
+		left_ctx = s_get_left_ctx(sp, 0);
+	char *ctx = s_get_ctx(sp);
+	char *right_ctx = s_get_right_ctx(sp);
+
+
+	log_debug("left ctx : %s", left_ctx);
+	log_debug("ctx      : %s", ctx);
+	log_debug("right ctx: %s", right_ctx);
+
+	if (left_ctx == NULL)
+		dp = opendir(".");
+	else
+		dp = opendir(left_ctx);
+	
+	if (dp != NULL) {
 		while ((ep = readdir (dp)))
 		{
-			if (ep->d_name[0] != '.')
+			if (ep->d_name[0] != '.' || ctx[0] == '.')
 			{
-				int match = check_globbing(arg, ep->d_name);
+				int match = check_globbing(ctx, ep->d_name);
 
 				if (match)
-					add_node_to_list(list_globbing, ep->d_name);
+				{
+					char *full_match;
 
+					if (left_ctx)
+						full_match = ft_strjoin(left_ctx, ep->d_name);
+					else
+						full_match = ft_strdup(ep->d_name);
+					if (right_ctx)
+					{
+						full_match = ft_strjoin(full_match, right_ctx);
+						s_globbing_interrogation_parse_argument(full_match, list_globbing);
+					}
+					else
+						add_node_to_list(list_globbing, full_match);
+				}
 				log_debug("=> %d -> %s with %s", match, arg, ep->d_name);
+
 			}
 		}
 		closedir(dp);
 	}
 	if (list_is_empty(list_globbing))
 	{
-		log_debug("globbing list is empty (filled with input");
+		log_debug("globbing list is empty (filled with input %s)", arg);
 		add_node_to_list(list_globbing, arg);
 	}
 	return (ST_OK);
