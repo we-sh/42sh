b5207b443260b76f45deff14510b182cfe4db5f5
abombard <arthbab@gmail.com>
2016-11-30 23:52:33 +0100

    implement !:n !:$ !:^ !:*

---
+++ incs/termcaps/events.h
@@ -0,0 +1,20 @@
+#ifndef EVENTS_H
+# define EVENTS_H
+
+# define WORD_ALL       -42
+# define WORD_ARGS      -2
+
+typedef struct	s_event
+{
+	size_t	token_size;
+    int     history_index;
+	int		word;
+}				t_event;
+
+int				replace_events(t_termcaps_context *context,
+								const size_t buffer_size_max,
+								t_buffer *buffer);
+int			event_info(t_list_head *history,
+						const char *pt,
+                        t_event *event);
+#endif
--- srcs/termcaps/events.c
+++ srcs/termcaps/events.c
@@ -1,5 +1,79 @@
 #include "shell.h"
 
+static int  s_next_word(char *s, size_t s_size, size_t *start, size_t *size)
+{
+    size_t  i;
+
+    i = 0;
+    while (i < s_size)
+    {
+        if (!ft_isspace(s[i]))
+            break ;
+        i++;
+    }
+    *start = i;
+    if (i == s_size)
+        return (0);
+    while (i < s_size)
+    {
+        if (ft_isspace(s[i]))
+            break ;
+        i++;
+    }
+    *size = i - *start;
+    log_debug("word {%.*s}", (int)*size, s + *start);
+    return (1);
+}
+
+static int  s_event_word(t_event *info, t_buffer *event)
+{
+    size_t      word_count;
+    size_t      start;
+    size_t      size;
+
+    log_debug("info->word %d", info->word);
+    word_count = 0;
+    if (info->word == WORD_ALL)
+        return (1);
+    if (info->word == WORD_ARGS)
+    {
+        s_next_word(event->bytes, event->size, &start, &size);
+        event->bytes += start + size;
+        event->size -= start + size;
+        s_next_word(event->bytes, event->size, &start, &size);
+        event->bytes += start;
+        event->size -= start;
+    }
+    else if (info->word == -1)
+    {
+        start = event->size;
+        while (start > 0 && !ft_isspace(event->bytes[start]))
+            start--;
+        if (start > 0)
+            start++;
+        event->bytes += start;
+        event->size -= start;
+    }
+    else if (info->word >= 0)
+    {
+        while (s_next_word(event->bytes, event->size, &start, &size))
+        {
+            if (word_count == (size_t)info->word)
+            {
+                event->bytes += start;
+                event->size = size;
+                return (1);
+            }
+            event->bytes += start + size;
+            event->size -= start + size;
+            word_count++;
+        }
+        return (0);
+    }
+    log_debug("Found event word {%.*s}", (int)event->size, event->bytes);
+    return (1);
+}
+
 #define EVENT_NOT_FOUND					"event not found"
 
 static int	s_replace_event(t_termcaps_context *context,
@@ -8,27 +82,27 @@ static int	s_replace_event(t_termcaps_context *context,
 							size_t buf_offset)
 {
 	char		*pt;
-	size_t		token_size;
-	int			history_index;
+    t_event     info;
 	t_buffer	event;
 	size_t		size_left;
 
 	pt = buf->bytes + buf_offset;
-	if (!event_info(&context->history, pt, &token_size, &history_index) ||
-		!history_get(&context->history, history_index, &event))
+	if (!event_info(&context->history, pt, &info) ||
+		!history_get(&context->history, info.history_index, &event) ||
+        !s_event_word(&info, &event))
 	{
-		termcaps_error(context, pt, token_size, EVENT_NOT_FOUND);
+		termcaps_error(context, pt, info.token_size, EVENT_NOT_FOUND);
 		return (0);
 	}
-	size_left = buf->size - buf_offset + token_size;
+	size_left = buf->size - buf_offset + info.token_size;
 	if (buf_offset + event.size + size_left >= buf_size_max)
 	{
 		termcaps_error(context, "", 0, "Command line too big");
 		return (0);
 	}
-	ft_memmove(pt + event.size, pt + token_size, size_left);
+	ft_memmove(pt + event.size, pt + info.token_size, size_left);
 	ft_memcpy(pt, event.bytes, event.size);
-	buf->size += event.size - token_size;
+	buf->size += event.size - info.token_size;
 	buf->bytes[buf->size] = '\0';
 	log_debug("New command {%.*s}", (int)buf->size, buf->bytes);
 	return (event.size);
