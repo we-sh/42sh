1b7b9742ff0f5a5c69183b1616ee366a39ae421a
abombard <arthbab@gmail.com>
2016-12-01 19:14:18 +0100

    norme

--- srcs/builtins/history/wrap_histfile.c
+++ srcs/builtins/history/wrap_histfile.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 
-void	wrap_histfile(t_sh *sh, int opt, char *filename, void (*fun)(t_sh *, int))
+void	wrap_histfile(t_sh *sh, int opt, char *filename,
+						void (*fun)(t_sh *, int))
 {
 	char	*histfile;
 
