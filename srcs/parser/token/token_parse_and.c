3585c75588e031964bfb6a25cdfe43b8a192c841
Jean-Michel Gigault <jgigault@student.42.fr>
2017-01-02 12:55:35 +0100

    fix parse error and background jobs

--- srcs/parser/token/token_parse_and.c
+++ srcs/parser/token/token_parse_and.c
@@ -4,6 +4,17 @@ static int	s_none(t_lexer *lexer, int *i)
 {
 	int		k;
 
+	k = *i + 1;
+	while (k < lexer->size)
+	{
+		if (TOKEN_TYPE(k) != TT_SEPARATOR && TOKEN_TYPE(k) != TT_NAME)
+		{
+			if (lexer->notify == 1)
+				display_status(ST_PARSER_TOKEN, NULL, TOKEN_CONTENT(k));
+			return (ST_PARSER);
+		}
+		k++;
+	}
 	k = *i - 1;
 	while (k >= 0 && TOKEN_TYPE(k) == TT_SEPARATOR)
 		k--;
