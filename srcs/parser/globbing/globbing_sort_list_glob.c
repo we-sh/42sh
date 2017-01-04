54bab8dc1389acd53b8a12d0bfb154c1d9a87639
abombard <arthbab@gmail.com>
2017-01-04 12:47:28 +0100

    sort_list

--- srcs/parser/globbing/globbing_sort_list_glob.c
+++ srcs/parser/globbing/globbing_sort_list_glob.c
@@ -27,9 +27,32 @@
 // 	exit(0);
 // }
 
+void		globbing_sort_list_glob(t_list **argv_list)
+{
+	t_argv	*argv_cur;
+	t_argv	*argv_next;
+	t_list	*pos;
+	size_t	size;
+	size_t	i;
+
+	size = list_size(*argv_list);
+	i = 1;
+	while (i < size)
+	{
+		pos = list_nth(*argv_list, i);
+		argv_cur = CONTAINER_OF(pos, t_argv, argv_list);
+		pos = list_nth(*argv_list, i + 1);
+		argv_next = CONTAINER_OF(pos, t_argv, argv_list);
+		if (ft_strcmp(argv_cur->buffer, argv_next->buffer) > 0)
+			list_move_tail(&argv_next->argv_list, &argv_cur->argv_list);
+		i++;
+	}
+}
+
+/*
 void        globbing_sort_list_glob(t_list **argv_list)
 {
-	t_list    *newlist;
+	t_list  *newlist;
 	t_list  *pos;
 	t_list  *safe;
 	t_list  *tmp;
@@ -71,3 +94,4 @@ void        globbing_sort_list_glob(t_list **argv_list)
 	log_info("OUT");
 	*argv_list = newlist;
 }
+*/
