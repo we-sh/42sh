1eff346f662ed9c7c11899ea74b10284a2b6484b
Adrien Nouvel <adrien.nouvel@outlook.com>
2017-01-07 10:04:32 +0100

    norme

--- srcs/parser/globbing/globbing.c
+++ srcs/parser/globbing/globbing.c
@@ -1,5 +1,54 @@
 #include "shell.h"
 
+static void	s_delete_argv(t_argv **arg)
+{
+	if (arg && *arg && (*arg)->buffer)
+	{
+		ft_strdel(&((*arg)->buffer));
+		free(*arg);
+		arg = NULL;
+	}
+}
+/*
+** Check whether a string contains globbing tokens.
+*/
+
+static int	s_globbing_is_matching_token(char *arg)
+{
+	if (!arg)
+	{
+		return (0);
+	}
+	if (ft_strchr(arg, '?') || ft_strchr(arg, '*') || ft_strchr(arg, '['))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+static void	s_globbing_proceed_on_token(t_list *list_glob, t_argv *arg)
+{
+	if (!list_glob || !arg || !(arg->buffer))
+	{
+		return ;
+	}
+	if (s_globbing_is_matching_token(arg->buffer))
+	{
+		globbing_run_parse(arg->buffer, list_glob);
+		if (list_is_empty(list_glob))
+		{
+			globbing_add_node_to_list(list_glob, arg->buffer);
+		}
+	}
+	else
+	{
+		globbing_add_node_to_list(list_glob, arg->buffer);
+	}
+}
+
 /*
 ** The purpose of this function is to browse the current argument list, and to
 ** perform the globbing on each of them.
@@ -14,34 +63,19 @@ int			globbing(t_list **argv_list)
 	t_list	*safe;
 
 	if ((list_glob = (t_list *)malloc(sizeof(t_list))) == NULL)
+	{
 		return (ST_MALLOC);
+	}
 	INIT_LIST_HEAD(list_glob);
 	safe = (*argv_list)->next;
 	while ((pos = safe) && pos != *argv_list)
 	{
 		safe = safe->next;
 		arg = CONTAINER_OF(pos, t_argv, argv_list);
-		log_info("proceed globbing on `%s'", arg->buffer);
-
-		if (ft_strchr(arg->buffer, '?') || ft_strchr(arg->buffer, '*') || ft_strchr(arg->buffer, '['))
-		{
-			globbing_run_parse(arg->buffer, list_glob);
-			if (list_is_empty(list_glob))
-				globbing_add_node_to_list(list_glob, arg->buffer);
-		}
-		else
-			globbing_add_node_to_list(list_glob, arg->buffer);
-
-		 log_info("Value of arg->buffer", arg->buffer);
-
-		if (arg->buffer)
-			free(arg->buffer);
-		if (arg)
-			free(arg);
+		s_globbing_proceed_on_token(list_glob, arg);
+		s_delete_argv(&arg);
 	}
-	log_info("before list_del(argv_list)");
 	free(*argv_list);
-	log_info("After list_del(argv_list)");
 	globbing_sort_list_glob(&list_glob);
 	*argv_list = list_glob;
 	return (ST_OK);
