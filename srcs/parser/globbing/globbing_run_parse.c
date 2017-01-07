1ae254f8095b97adb1a7b0facfb760ca8545e84d
Adrien Nouvel <adrien.nouvel@outlook.com>
2017-01-07 10:47:49 +0100

    remove log messages

--- srcs/parser/globbing/globbing_run_parse.c
+++ srcs/parser/globbing/globbing_run_parse.c
@@ -14,7 +14,6 @@ int	check_globbing(char *pattern, char *input)
 	is_valid = 0;
 	if (!pattern || !input)
 		return (0);
-	log_error("Pattern:%s, input:%s",pattern, input);
 	if (*pattern == '?')
 	{
 		return (*input && check_globbing(pattern + 1, input + 1));
@@ -59,24 +58,17 @@ void				globbing_run_parse(char *arg, t_list *list_glob)
 	t_mylist *tmplist;
 
 	list = NULL;
-	log_debug("%s", arg);
 	globbing_load_context(&c, arg);
-	log_debug("left context   : `%s'", c->l);
-	log_debug("middle context : `%s'", c->m);
-	log_debug("right context  : `%s'", c->r);
 	if (!(c->m))
 		globbing_add_node_to_list(list_glob, arg);
 	else
 	{
 		if ((dp = (!(c->l)) ? opendir(".") : opendir(c->l)))
 		{
-			log_info("Before while readdir");
 			while ((e = readdir(dp)))
 			{
-				//log_success("turn  : `%d' -> %s", i,e->d_name);
 				if (e->d_name[0] != '.' || (c->m && c->m[0] == '.'))
 				{
-					log_debug("\tWHILE readit(dp) performing globbing on (middle context) `%s' \n\t\t\t\twith (file name) `%s'", c->m, e->d_name);
 				
     	    		if (globbing_bracket(&list, c->m, e->d_name) == -1)
     	    		{
@@ -84,47 +76,35 @@ void				globbing_run_parse(char *arg, t_list *list_glob)
 						return ;
     	    		}
 
-					//s_iter_on_list(list, c, list_glob, e, &i, arg);	
 						int ret;
 						char      *m;
 
 						ret = 0;
 						while (list)
 						{
-							log_debug("after globbing_bracket list->content:%s", list->content);
-							log_debug("after globbing_bracket e->d_name:%s", e->d_name);
 							if ((ret = check_globbing(list->content, e->d_name)) > 0)
 							{
-								log_debug("Value of RET:%d", ret);
 								if (c->l)
 									m = ft_strjoin(c->l, e->d_name);
 								else
 									m = ft_strdup(e->d_name);
 
-								log_info("\t2performing globbing on (middle context) `%s' \n\t\t\t\twith (file name) `%s' => produce `%s'", c->m, e->d_name, m);
-								log_info("c->l:%s", c->l);
-								log_info("c->m:%s", c->m);
-								log_info("c->r:%s", c->r);
 								if (c->r)
 								{
-									log_info("Recurse: %s",m);
 								 globbing_run_parse(m = s_join_free(m, c->r), list_glob);
 								}
 								else
 								{
-									log_info("Add_node_to_list %s", m);
 									globbing_add_node_to_list(list_glob, m);
 								}
 								ft_strdel(&m);
-								i += 1;//debug
+								i += 1;
 							}
-							else if (ret == -1)// ret of check-globing equal -1 so we didn't replace bracket and return input
+							else if (ret == -1)
 							{
-									log_debug("Value of RET:%d", ret);
 									globbing_add_node_to_list(list_glob, arg);
 									return ;
 							}
-							log_debug("Value of RET:%d", ret);
 							ft_strdel(&(list->content));
 							tmplist = list->next;
 							free(list);
@@ -134,15 +114,10 @@ void				globbing_run_parse(char *arg, t_list *list_glob)
 			}
 			if (i <= 0)
 			{
-				log_info("RET=%d..et a toujours ete 0");
 				globbing_add_node_to_list(list_glob, arg);
 			}
 			closedir(dp);
 		}
 	}
-	//if (list_is_empty(list_glob) && dp != NULL && (ft_strchr(arg, '?') || ft_strchr(arg, '*')))
-	// if (list_is_empty(list_glob) && (ft_strchr(arg, '?') || ft_strchr(arg, '*')))
-	//  	globbing_add_node_to_list(list_glob, arg);
   globbing_context_delete(c);
-  
 }
