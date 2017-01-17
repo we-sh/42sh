4c217461b74ca3b7be8d8b55bf5f6dad3e015014
Jean-Michel Gigault <jgigault@student.42.fr>
2017-01-17 16:26:08 +0100

    fix norme + inhibited closing bracket

--- srcs/parser/expand/expand_get_words_special_build_str.c
+++ srcs/parser/expand/expand_get_words_special_build_str.c
@@ -1,22 +1,34 @@
 #include "shell.h"
 
+static int	s_is_inhibited(char *start, char *c)
+{
+	int		odd;
+
+	odd = 0;
+	while (c != start)
+	{
+		c--;
+		if (c[0] != '\\')
+			return (odd);
+		odd = (odd == 0) ? 1 : 0;
+	}
+	return (odd);
+}
+
 static void	s_build_str_2_get_char(char *str, char *input, int *k)
 {
 	int		l;
 	int		has_closing_bracket;
 
-	if (input[*k] == '*' || input[*k] == '?')
-	{
-		str[*k] = -input[*k];
-		return ;
-	}
 	str[*k] = input[*k];
 	l = 1;
 	has_closing_bracket = 0;
 	while (input[*k + l] != '\0')
 	{
 		str[*k + l] = input[*k + l];
-		if (l > 1 && input[*k + l] == ']' && !(l == 2 && input[*k + l - 1] == '^'))
+		if (l > 1 && input[*k + l] == ']'
+			&& !(l == 2 && input[*k + l - 1] == '^')
+			&& s_is_inhibited(input, input + *k + l) == 0)
 		{
 			has_closing_bracket = l;
 			break ;
@@ -40,8 +52,10 @@ char		*expand_get_words_special_build_str(char *content, char *input)
 	k = 0;
 	while (input[k] != '\0')
 	{
-		if (input[k] == '[' || input[k] == '*' || input[k] == '?')
+		if (input[k] == '[')
 			s_build_str_2_get_char(str, input, &k);
+		else if (input[k] == '*' || input[k] == '?')
+			str[k] = -input[k];
 		else
 			str[k] = input[k];
 		k++;
