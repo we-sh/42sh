dceca4b162317d335fab3872f378a80b325ed566
Jean-Michel Gigault <jgigault@student.42.fr>
2017-01-20 18:11:44 +0100

    fix EINVAL with main.c

--- srcs/main.c
+++ srcs/main.c
@@ -8,25 +8,15 @@ t_list		g_current_jobs_list_head = {
 static int	s_usage(int status)
 {
 	int		fd;
-	int		i;
 
 	fd = status == ST_OK ? STDOUT_FILENO : STDERR_FILENO;
 	if (status != ST_OK)
 		ft_printf("%r%s: %s\n", fd,
 			PROGRAM_NAME,
 			i18n_translate(status));
-	ft_printf("%r%s: %s [--help] [-c STRING]\n%s\n", fd,
+	ft_printf("%r%s: %s\n", fd,
 		i18n_translate(ST_USAGE),
-		PROGRAM_NAME,
-		i18n_translate(ST_OPTIONS));
-	i = 0;
-	while (g_sh_options[i])
-	{
-		ft_printf("%r `%s`: %s\n", fd,
-			g_sh_options[i]->name,
-			i18n_translate(g_sh_options[i]->index));
-		i++;
-	}
+		PROGRAM_NAME);
 	return (EXIT_SUCCESS);
 }
 
@@ -49,10 +39,11 @@ int			main(int argc, char *argv[], char *envp[])
 	int		ret;
 	t_sh	sh;
 
-	(void)argc;
 	logger_init(D_TRACE, "/tmp/out.log");
 	if ((sh.argv = ft_array_dup(argv)) == NULL)
 		return (display_status(ST_MALLOC, NULL, NULL));
+	if (argc > 1)
+		return (s_usage(ST_EINVAL));
 	if ((ret = option_parse(&sh.opt_head, g_sh_options, &sh.argv, 1)) != ST_OK)
 		return (s_usage(ret));
 	if (option_is_set(&sh.opt_head, ST_OPTION_HELP) == 1)
