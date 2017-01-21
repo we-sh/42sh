12f41419d669df9d6f8eab3938ae097207fce948
abombard <arthbab@gmail.com>
2017-01-21 12:43:17 +0100

    ctrl C works anytime

--- srcs/termcaps/key__ctrl_c.c
+++ srcs/termcaps/key__ctrl_c.c
@@ -2,12 +2,10 @@
 
 int		key__ctrl_c(t_termcaps_context *context)
 {
-	if (context->state != STATE_REGULAR &&
-		context->state != STATE_SELECTION)
-		return (1);
-	if (context->state == STATE_SELECTION)
+	if (context->state != STATE_REGULAR)
 	{
-		termcaps_write(context->fd, SELECTBLANC, sizeof(SELECTBLANC) - 1);
+		if (context->state == STATE_SELECTION)
+			termcaps_write(context->fd, SELECTBLANC, sizeof(SELECTBLANC) - 1);
 		context->state = STATE_REGULAR;
 	}
 	ASSERT(termcaps_display_command(context));
